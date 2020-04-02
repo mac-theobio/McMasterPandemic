c16f3fdcf95013df21621c5ab02fad57a53dd79a
David Earn <earn@math.mcmaster.ca>
2020-04-02 14:40:31 -0400

    package name

---
+++ R/pkgname.R
@@ -0,0 +1,21 @@
+##
+## vignette("rd",package="roxygen2")
+##
+
+##' \pkg{McMasterPandemic}
+##'
+##' This R package provides Compartmental epidemic models for
+##' forecasting and analysis of infectious disease pandemics.
+##' 
+##' See the vignette \code{McMasterPandemic.pdf} for now...
+##' Eventually \code{vignette("McMasterPandemic",package="McMasterPandemic")}
+##' should work...
+##' 
+##' \deqn{ dS/dt = - (\beta_0 / N) S (C_a I_a + C_p I_p + (1-iso_m)C_m I_m + (1-iso_s)C_s I_s) }
+##' \deqn{ dE/dt =  }
+##' \deqn{ ... }
+##' \deqn{ dR/dt =  }
+##'
+##' Use \code{browseVignettes("McMasterPandemic")} to find vignettes
+##' for this package.
+"_PACKAGE"
