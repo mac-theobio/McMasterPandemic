24e78d413274d554f6c56dd482a2939ececb213c
Ben Bolker <bbolker@gmail.com>
2020-04-05 13:03:52 -0400

    tests

---
+++ tests/AAAtest-all.R
@@ -0,0 +1,10 @@
+if(require("testthat")) {
+    pkg <- "McMasterPandemic"
+    require(pkg, character.only=TRUE)
+    print(sessionInfo())
+    test_check(pkg, reporter="summary")
+    print(warnings()) # TODO? catch most of these by expect_warning(..)
+} else {
+    warnings("Package 'testthat' not available, cannot run unit tests for package",
+	     sQuote(pkg))
+}
