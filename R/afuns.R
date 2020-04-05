6d06a34aa67c70023f3f7326728495dad0c92148
Jonathan Dushoff <dushoff@mcmaster.ca>
2020-04-05 12:45:18 -0400

    merge with Ben's progress (working on fixing moments)

--- R/afuns.R
+++ R/afuns.R
@@ -22,10 +22,11 @@ get_r <- function(p) {
 get_GI_moments <- function(params) {
     ## FIXME: assumes ICU1 model. Consider adding a test in case this changes?
     ##  (will have to rethink this once we have a structured model)
-    Rv <- get_R0(params, components=TRUE)
+    Rv <- c(0, get_R0(params, components=TRUE))
     R <- sum(Rv)
-    irates <- with(as.list(params), c(lambda_a, lambda_p, lambda_m, lambda_s))
-    gamma  <- params[["gamma"]]
+    boxrates <- with(as.list(params), 
+	 	c(gamma, lambda_a, lambda_p, lambda_m, lambda_s)
+	)
 
     Gibar <- sum((Rv/R)/irates)
     Givar <- sum((Rv/R)/irates^2) - Gibar^2
