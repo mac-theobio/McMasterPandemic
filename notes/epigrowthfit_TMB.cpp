559d618f20326af53addafdfc8543136bef56b5d
Ben Bolker <bbolker@gmail.com>
2020-04-11 21:38:33 -0400

    working on TMB time-series fits

--- notes/epigrowthfit_TMB.cpp
+++ notes/epigrowthfit_TMB.cpp
@@ -1,22 +1,27 @@
+#include <TMB.hpp>
+#include <iostream>
+#include <string>
+
 // eventual goal: flexible epigrowthfit-style fitting
 // possibility of including multiple time series with random effects parameters
 // compare cumulative vs incidence fitting?
 // regularization? priors?
 
+// https://github.com/kaskr/adcomp/issues/59
+template<class Type>
+bool isNA(Type x){
+  return R_IsNA(asDouble(x));
+}
 
-#include <TMB.hpp> // for print statements
-#include <iostream>
-#include <string>
+
 
 // https://kaskr.github.io/adcomp/_book/Tutorial.html
 template<class Type>
 Type objective_function<Type>::operator() ()
 {
   // Data
-  DATA_VECTOR(t0);     // times (includes step [-1])
+  DATA_VECTOR(t);      // times (includes step [-1])
   DATA_VECTOR(x);      // initial densities
-  DATA_SCALAR(dt);     // time step
-
   
   // Parameters  
   PARAMETER(log_thalf);    // log half-max time
@@ -25,7 +30,7 @@ Type objective_function<Type>::operator() ()
   PARAMETER(log_p);        // log Richards shape parameter
   PARAMETER(log_nb_disp);  // log NB dispersion parameter (FIXME: reparameterize?)
 
-  vector<Type> cumcurve(x.size()+1);
+  vector<Type> cumcurve(t.size()); // includes starting time
   vector<Type> inccurve(x.size());
   
   // Objective funcction
@@ -39,14 +44,18 @@ Type objective_function<Type>::operator() ()
   Type nb_disp = exp(log_nb_disp); 
   
   // parameter est
-  
-  for (int i = -1; i < x.size(); i++) {
-    cumcurve(i+1) = K/(1+exp(-r*(i*dt-thalf)));
+
+  for (int i = 0; i < t.size(); i++) {
+    cumcurve(i) = K/(1+exp(-r*(t(i)-thalf)));
   }
   for (int i = 0; i < x.size(); i++) {
     inccurve(i) = cumcurve(i+1)-cumcurve(i);
-    jnll -= dnbinom2(x(i),inccurve(i),nb_disp,1);
+    if (!isNA(x(i))) {
+      // std::cout << i << " " << x(i) << " " << inccurve(i)  << std::endl;      
+      jnll -= dnbinom2(x(i),inccurve(i),nb_disp,1);
+    }
   }
   
+  REPORT(inccurve);
   return jnll;
 }
