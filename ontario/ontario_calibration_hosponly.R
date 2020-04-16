a3f1c7f18c3f67c1e3e1ab2f888c82bd6afd58c3
Ben Bolker <bbolker@gmail.com>
2020-04-16 16:56:16 -0400

    process-error sims; cleanup

---
+++ ontario/ontario_calibration_hosponly.R
@@ -0,0 +1,12 @@
+library(McMasterPandemic)
+
+## use hospitalization data only
+ont_recent_hosp <- na.omit(dplyr::filter(ont_all_sub, var=="H"))
+opt_pars_2brk <- opt_pars
+opt_pars_2brk$log_rel_beta0 <- rep(-1,2)  ## only two breakpoints (hosp data doesn't even start until after brk 1)
+bd2 <- bd[-1]
+## don't try to estimate fraction mild (= !hospital, mu) or fraction in acute care (= !ICU, phi1)
+opt_pars_2brk$params <- opt_pars_2brk$params[c("log_E0","log_beta0")]
+ont_cal2 <- update(ont_cal1,  data=ont_recent_hosp, opt_pars=opt_pars_2brk, break_dates=bd2)
+
+# rdsave("ont_cal2", "opt_pars_2brk", "bd2")
