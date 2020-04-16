2e5ba5c12def15610465c977e37fe257a466ad5b
Ben Bolker <bbolker@gmail.com>
2020-04-15 20:36:18 -0400

    add hosp vs all plot

--- output/ontario_cal_plots.R
+++ output/ontario_cal_plots.R
@@ -5,19 +5,20 @@ library(ggplot2); theme_set(theme_bw())
 library(directlabels)
 
 keep_vars <- c("H","ICU","d","incidence","report","newTests/1000")
-f_args <- attr(ont_cal1,"forecast_args")
+
 
 get_type <- . %>%  mutate(vtype=ifelse(var %in% c("incidence","report","d"),
                                        "inc","prev"))
 sub_vars <- . %>% dplyr::filter(var %in% keep_vars)
 
 ## make forecast with specified end date
-mk_fc <- function(new_end=NULL) {
+mk_fc <- function(new_end=NULL,fit=ont_cal1) {
+    f_args <- attr(fit, "forecast_args")
     if (!is.null(new_end)) {
         f_args$end_date <- new_end
     }
     fc <- (do.call(forecast_sim,
-                      c(list(p=ont_cal1$par), f_args))
+                      c(list(p=fit$par), f_args))
         %>% sub_vars()
         %>% get_type()
     )
@@ -49,15 +50,14 @@ capac_info <- data.frame(value=c(630,1300),
 plotfun <- function(forecast, data=ont_all_sub, breaks=bd,
                     dlspace=1,
                     limspace=10,
-                    add_tests=TRUE,
-                    add_ICU_cap=FALSE) {
+                    add_tests=FALSE,
+                    add_data=TRUE,
+                    add_ICU_cap=FALSE,
+                    mult_var=NULL) {
     data <- sub_vars(data)
     if (!add_tests) data <- filter(data,var!="newTests/1000")
     p <- (ggplot(forecast,aes(date,value,colour=var))
-        + geom_line()
         + scale_y_log10(limits=c(1,NA),oob=scales::squish)
-        + geom_point(data=data)
-        + geom_line(data=data,alpha=0.2)
         + geom_vline(xintercept=breaks,lty=2)
         + facet_wrap(~vtype,ncol=1,scale="free_y")
         + labs(y="")
@@ -66,6 +66,16 @@ plotfun <- function(forecast, data=ont_all_sub, breaks=bd,
                 strip.background = element_blank(),
                 strip.text = element_blank())
     )
+    if (is.null(mult_var)) {
+        p <- p + geom_line()
+    } else {
+        p <- p + geom_line(aes_string(lty=mult_var))
+    }
+    if (add_data) {
+        p <- (p + geom_point(data=data)
+            + geom_line(data=data,alpha=0.2)
+        )
+    }
     ## trying to fix spacing on the fly: kluge!
     ## dlspace not found
     ## geom_dl() must do weird evaluation env stuff
@@ -86,19 +96,24 @@ plotfun <- function(forecast, data=ont_all_sub, breaks=bd,
            )
 }
 
+save_both <- function(gg, fn) {
+    ggsave(gg, file=sprintf("output/%s.pdf",fn))
+    ggsave(gg, file=sprintf("output/%s.png",fn))
+}
 forecast1 <- mk_fc()
-gg_cal1 <- plotfun(forecast1,limspace=15)
-ggsave(gg_cal1, file="output/ont_cal1.pdf")
-ggsave(gg_cal1, file="output/ont_cal1.png")
-
+gg_cal1 <- plotfun(forecast1,limspace=15,add_tests=TRUE)
+save_both(gg_cal1,"ont_cal1")
 
 forecast2 <- mk_fc(new_end="2020-05-15")
-gg_cal2 <- plotfun(forecast2,add_tests=FALSE,add_ICU_cap=TRUE,limspace=20)
-ggsave(gg_cal2, file="output/ont_cal2.pdf")
-ggsave(gg_cal2, file="output/ont_cal2.png")
+gg_cal2 <- plotfun(forecast2,add_ICU_cap=TRUE,limspace=20)
+save_both(gg_cal2,"ont_cal2")
 
 forecast3 <- mk_fc(new_end="2020-10-01")
-gg_cal3 <- plotfun(forecast3,add_tests=FALSE,limspace=45)
-ggsave(gg_cal3, file="output/ont_cal3.pdf")
-ggsave(gg_cal3, file="output/ont_cal3.png")
+gg_cal3 <- plotfun(forecast3,limspace=45)
+save_both(gg_cal3,"ont_cal3")
 
+## hospitalization data only
+forecast4 <- mk_fc(fit=ont_cal2)
+comb_fc <- dplyr::bind_rows(list(all=forecast1,H_only=forecast4),.id="calib_vars")
+gg_cal4 <- plotfun(comb_fc, mult_var="calib_vars")
+save_both(gg_cal4,"ont_cal4")
