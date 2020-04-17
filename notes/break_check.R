31b9c99711343e0d79728e1edc22eb41b14f94f6
wzmli <wzmichael.li@gmail.com>
2020-04-17 01:42:45 -0400

    finished break_check example

--- notes/break_check.R
+++ notes/break_check.R
@@ -1,38 +1,46 @@
-## example
-
-
+## This test break points where it changes beta0
+## We are assuming the effect to happen immediately right after the break date to incidence
+## 
 library(McMasterPandemic)
 library(tidyverse)
 library(zoo)
 library(anytime)
-library(ggplot2)
+library(ggplot2); theme_set(theme_bw())
 
+##  load parameters
+params <- fix_pars(read_params("ICU1.csv")
+   , target=c(Gbar=6)
+   , pars_adj=list(c("sigma","gamma_s","gamma_m","gamma_a"))
+)
+                   
 first_date <- as.Date("2020-03-15")
 end_date <- as.Date("2020-03-25")
-dd <- data.frame(date = as.Date(first_date:end_date)
-    , value = c(100,105,120,160,200,300,200,150,200,300,350)
-    # , value = c(30,300,40,50,60,65,70,60,45,55,50)
-    , var = "newConfirmations"
-)
-
-print(dd)
-
-## 
-params <- fix_pars(read_params("ICU1.csv")
-                   , target=c(Gbar=6)
-                   , pars_adj=list(c("sigma","gamma_s","gamma_m","gamma_a"))
-)
-params[["N"]] <- 19.5e6  ## reset pop to Ontario
 
 ## breakpoints
-change1 <- "20-Mar-2020"
-change2 <- "22-Mar-2020"
+change1 <- anydate(first_date + 19)
+change2 <- anydate(first_date + 29)
 
+dd <- data.frame(date = as.Date(first_date:(first_date+39))
+   , I = c(seq(2,2000,length.out = 20)
+      , seq(1800,1500,length.out=10)
+      , seq(1600,5000,length.out=10)
+      )
+   , var = "report"
+)
 
+## convoluting incidence to get report
+## we need to round because we are fitting negbinom and getting rid of the NAs in the beginning
+dd$value <- round(calc_conv(dd$I,params))
+dd <- dd %>% filter(!is.na(value))
+
+## setting some population size
+params[["N"]] <- 10e6  
+
+## The break points actually don't match the reports because we want to match incidence
 (ggplot(dd,aes(x=date,y=value))
     + geom_line()
     + geom_point()
-    + geom_vline(xintercept=as.Date(c("2020-03-20","2020-03-22")))
+    + geom_vline(xintercept=as.Date(c(change1,change2)))
 )
 
 bd <- anydate(c(change1,change2))
@@ -55,52 +63,24 @@ opt_pars <- list(
     log_nb_disp=0)
 
 t1 <- system.time(g1 <- calibrate(data=dd
-                                  , base_params=params
-                                  , optim_args=list(control=list(maxit=10000),hessian=TRUE)
-                                  , opt_pars = opt_pars
-                                  , break_dates = bd
-                                  , debug=TRUE
-                                  , debug_plot = TRUE
+   , base_params=params
+   , optim_args=list(control=list(maxit=10000),hessian=TRUE)
+   , opt_pars = opt_pars
+   , break_dates = bd
+   # , debug=TRUE
+   # , debug_plot = TRUE
 )
 ) ## system.time
 
 
 f_args <-attr(g1,"forecast_args")
 i1 <- invlink_trans(restore(g1$par,f_args$opt_pars))
-params_fitted <- update(params,i1$params)
-## hack to get names of params used to adjust Gbar
-moment_params <- eval(formals(fix_pars)$pars_adj)[[2]]
-out <- (describe_params(params_fitted)
-        %>% mutate(type=case_when(
-            symbol %in% names(i1$params) ~ "mle-calibrated",
-            symbol %in% moment_params ~ "Gbar-calibrated",
-            TRUE ~ "assumed"),
-            type=factor(type,levels=c("mle-calibrated",
-                                      "Gbar-calibrated",
-                                      "assumed")))
-        %>% arrange(type)
-)
 
-get_type <- . %>%  mutate(vtype=ifelse(var %in% c("incidence","report","newTests"),
-                                       "inc","prev"))
-mk_fc <- function(new_end=NULL) {
-    if (!is.null(new_end)) {
-        f_args$end_date <- new_end
-    }
-    fc <- (do.call(forecast_sim,
-                   c(list(p=g1$par), f_args))
-           # %>% dplyr::filter(var %in% keep_vars)
-           %>% get_type()
-    )
-    return(fc)
-}
-forecast1 <- mk_fc()
-
-aa <- forecast1 %>% filter(var %in% c("report","incidence"))
-(ggplot(aa,aes(x=date,y=value,colour=var))
+fc <- (do.call(forecast_sim,c(list(p=g1$par), f_args))) 
+forecast <- fc %>% filter(var %in% c("report","incidence"))
+(ggplot(forecast,aes(x=date,y=value,colour=var))
     + geom_line()
-    + geom_vline(xintercept = as.Date("2020-03-20"))
+    + geom_vline(xintercept = as.Date(c(change1,change2)))
     + scale_x_date(breaks="2 day")
     + scale_y_log10()
 )
- 
