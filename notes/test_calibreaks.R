0f8b8265915a1ac04f0ffd1194bbf2890f70db1b
wzmli <wzmichael.li@gmail.com>
2020-04-21 14:59:24 -0400

    pushing simulation test code

--- notes/test_calibreaks.R
+++ notes/test_calibreaks.R
@@ -3,41 +3,23 @@ library(ggplot2)
 library(tidyverse)
 library(anytime)
 
+## setup 
+
 params <- fix_pars(read_params("ICU1.csv"))
+params[["obs_disp"]] <- 5
 
 start_date <- anydate("2020-01-01")
 end_date <- anydate("2020-04-01")
+
+# cutoff_start <- anydate("2020-02-10")
+# cutoff_end <- anydate("2020-03-10")
+
+cutoff_start <- anydate("2020-01-01")
+cutoff_end <- anydate("2020-04-01")
+
 break1 <- "2020-02-22"
 bd <- anydate(c(break1))
 rel_break1 <- 0.2
-
-params[["N"]] <- 1e7
-
-sim1break <- run_sim_break(params
-   , start_date=start_date
-   , end_date=end_date
-   , break_dates = bd
-   , rel_beta0= rel_break1
-     )
-plot(sim1break)
-
-simdat <- pivot(condense(sim1break))
-simdat <- simdat %>% filter(var %in% c("report","I","incidence"))
-print(ggsim <- ggplot(simdat,aes(x=date,y=value,color=var))
-   + geom_line()
-   + geom_vline(xintercept = bd)
-   + scale_y_log10()
-   + geom_point()
-)
-
-## What is wrong with the break date? Why isn't it turning at the break?
-
-## Need to round value because of negative binomial fit
-dd <- (simdat 
-   %>% filter(var == "report") 
-   %>% filter(!is.na(value)) 
-   %>% mutate(value = round(value))
-)
 
 opt_pars <- list(
    ## these params go to run_sim
@@ -45,17 +27,73 @@ opt_pars <- list(
    , log_rel_beta0 = rep(-1, length(bd))
    , log_nb_disp=0
 )
+params[["N"]] <- 1e7
+
+nsim <- 20
+
+sim_cali <- function(x){
+
+   set.seed(x)
+   sim1break <- run_sim_break(params
+      , start_date=start_date
+      , end_date=end_date
+      , break_dates = bd
+      , rel_beta0= rel_break1
+      , stoch = c(obs = TRUE, proc=FALSE)
+      )
+
+   simdat <- pivot(condense(sim1break))
+   simdat <- simdat %>% filter(var %in% c("report"))
+
+## Need to round value because of negative binomial fit
+dd <- (simdat 
+   %>% filter(!is.na(value)) 
+   %>% mutate(value = round(value))
+   %>% filter(between(date,cutoff_start, cutoff_end))
+)
 
 g1 <- calibrate(data=dd, base_params=params
    , opt_pars = opt_pars
    , break_dates = bd
 )
 
-pp <- invlink_trans(restore(g1$mle2@coef,opt_pars))
+res_dat <- data.frame(bbmle::confint(g1$mle2, method="quad", level=0.95)
+   , estimate = bbmle::coef(g1$mle2)
+   , seed = x
+   , pars = names(g1$mle2@coef)
+)
 
-print(pp)
+return(res_dat)
+}
 
-print(rel_break1)
+ll <- lapply(1:nsim,function(x)sim_cali(x))
 
-print(plot(g1))
-print(ggsim)
+truedf <- data.frame(pars = c("params.log_E0","params.log_beta0","log_rel_beta0","log_nb_disp")
+   , trueval = c(log(params[["E0"]]), log(params[["beta0"]]), log(rel_break1), log(params[["obs_disp"]]))
+)
+
+simdf <- (bind_rows(ll)
+   %>% left_join(.,truedf)  
+   %>% rowwise()
+   %>% transmute(pars
+         , trueval
+         , estimate
+         , lwr = X2.5..
+         , upr = X97.5..
+         , inCI = between(trueval, lwr, upr)
+         , seed
+      )
+   %>% ungroup()
+   %>% group_by(pars)
+   %>% mutate(ind = rank(estimate))
+)
+
+ggmilli <- (ggplot(simdf, aes(x=ind,y=estimate,color=inCI))
+   + geom_point()
+   + geom_pointrange(aes(ymin=lwr,ymax=upr))
+   + geom_hline(aes(yintercept = trueval))
+   + facet_wrap(~pars,scale="free")
+   + theme_bw()
+   + scale_color_manual(values=c("red","blue","black"))
+)
+print(ggmilli)
