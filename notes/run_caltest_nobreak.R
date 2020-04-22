ed2c36b508009c6cdbf8b55288e6600bdb57cd9b
Jonathan Dushoff <dushoff@mcmaster.ca>
2020-04-21 23:02:44 -0400

    night

--- notes/run_caltest_nobreak.R
+++ notes/run_caltest_nobreak.R
@@ -1,92 +1,60 @@
 library(McMasterPandemic)
-library(ggplot2)
 library(tidyverse)
 library(anytime)
 
 use_true_start <- TRUE
-nsim <- 30
+nsim <- 1
 ## setup 
 
 params <- fix_pars(read_params("ICU1.csv"))
+
+params[["beta0"]] <- 2
 params[["obs_disp"]] <- 100 ## BMB: less noise
 
 start_date <- anydate("2020-01-01")
 end_date <- anydate("2020-03-31") ## BMB: don't run as long
 
-break1 <- NULL
-bd <- NULL
-
-if (use_true_start) {
-   opt_pars <- list(
-      params=c(log_beta0=log(params[["beta0"]]))
-      , log_nb_disp = log(params[["obs_disp"]])
-   )
-}  else {
-   opt_pars <- list(
-      ## these params go to run_sim
-      params=c(log_E0=4, log_beta0=-1)
-      , log_rel_beta0 = rep(-1, length(bd))
-      , log_nb_disp=0
-   )
-}
+## Start with true parameters
+opt_pars <- list(
+	params=c(log_beta0=log(3))
+	, log_nb_disp = log(params[["obs_disp"]])
+)
 
 params[["N"]] <- 1e7
 
+seed=2222
+set.seed(seed)
+simdat <- run_sim(params
+	, start_date=start_date
+	, end_date=end_date
+	, stoch = c(obs = TRUE, proc=FALSE)
+)
 
-sim_cali <- function(x){
-   set.seed(x)
-   cat("seed ",x,"\n")
-   suppressWarnings(simdat <- run_sim(params
-      , start_date=start_date
-      , end_date=end_date
-      , stoch = c(obs = TRUE, proc=FALSE)
-   )
-   )
-   
-   ## plot(sim1break,log=TRUE)
-   simdat <- (simdat
-              %>% condense()
-              %>% pivot()
-              %>% filter(var %in% c("report"))
-   )
-   
-   
-   ## Need to round value because of negative binomial fit
-   dd <- (simdat 
-          %>% filter(!is.na(value)) 
-          %>% mutate(value = round(value))
-          # %>% filter(between(date,cutoff_start, cutoff_end))
-   )
-   
-   g1 <- calibrate(data=dd, base_params=params
-                   , start_date = start_date
-                   , opt_pars = opt_pars
-                   , break_dates = NULL
-                   , debug_plot=TRUE
-   )
-   
-   pp <- predict(g1)
-   
-   if (FALSE) {
-      plot(sim1break, keep_state=c("incidence","report"),log=TRUE) +
-         geom_line(data=filter(pp,var=="report"),lty=2)
-      
-      (ggplot(simdat, aes(date,value))
-         + scale_y_log10()
-         + geom_line()
-         + geom_line(data=filter(pp,var=="report"),lty=2)
-      )
-   }
-   
-   res_dat <- data.frame(bbmle::confint(g1$mle2, method="quad", level=0.95)
-                         , estimate = bbmle::coef(g1$mle2)
-                         , seed = x
-                         , pars = names(g1$mle2@coef)
-   )
-   return(list(simdat=simdat,fit=g1,pars=res_dat,pred=pp))
-}
+simdat <- (simdat
+	%>% condense()
+	%>% pivot()
+	%>% filter(var %in% c("report"))
+	%>% filter(!is.na(value)) 
+	%>% mutate(value = round(value))
+)
 
+print(params)
+print(opt_pars)
+g1 <- calibrate(data=simdat, base_params=params
+	, start_date = start_date
+	, opt_pars = opt_pars
+	, break_dates = NULL
+	, debug_plot=TRUE
+)
 
-## mclapply()
-res <- lapply(1:nsim, function(x)sim_cali(x))
-#rdsave(res)
+print(g1)
+
+pp <- predict(g1)
+
+res_dat <- data.frame(bbmle::confint(g1$mle2, method="quad", level=0.95)
+	, estimate = bbmle::coef(g1$mle2)
+	, seed = seed
+	, pars = names(g1$mle2@coef)
+)
+print(res_dat)
+
