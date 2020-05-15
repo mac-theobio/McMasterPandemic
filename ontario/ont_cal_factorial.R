80b6a285a130c0e2a34adfa87727bfd9fbb3c952
wzmli <wzmichael.li@gmail.com>
2020-05-14 23:39:19 -0400

    running the factorial design

--- ontario/ont_cal_factorial.R
+++ ontario/ont_cal_factorial.R
@@ -2,14 +2,9 @@ library(McMasterPandemic)
 library(splines)
 library(dplyr)
 
-## flags
-use_mobility <- FALSE
-use_spline <- FALSE
-use_zeta <- TRUE
-use_DEoptim <- FALSE
-
 load("ontario_clean.RData")
 load("ont_cal.RData")  ## baseline calibration: ont_cal1 (calibrated object), bd (breakpoint dates)
+
 
 ## select the part of the mobility data that lies within the calibration data set
 comb_sub2 <- (comb_sub
@@ -28,7 +23,7 @@ comb_sub3 <- (full_join(unique(select(ont_noICU,date)),comb_sub2,by="date")
     %>% mutate(t_vec=as.numeric(date-min(date)))
 )
 
-plot(rel_activity~date,data=comb_sub3)
+# plot(rel_activity~date,data=comb_sub3)
 
 opt_pars <- list(
     ## these params are part of the main parameter vector: go to run_sim()
@@ -44,6 +39,17 @@ opt_pars <- list(
   , log_nb_disp=NULL
 )
 
+
+run_cali <- function(x){
+
+print(x)
+
+## flags
+use_DEoptim <-as.numeric(substr(x,1,1))
+use_mobility <- as.numeric(substr(x,1,1))
+use_spline <- as.numeric(substr(x,2,2))
+use_zeta <- as.numeric(substr(x,3,3))
+
 ## now modify opt_pars according to flags
 if (use_zeta) opt_pars$params <- c(opt_pars$params,log_zeta=-1)
 loglin_terms <- "-1"
@@ -52,7 +58,7 @@ if (use_spline) loglin_terms <- c(loglin_terms, "bs(t_vec,df=round(length(t_vec)
 form <- reformulate(loglin_terms)
 X <- model.matrix(form, data = comb_sub3)
 
-matplot(comb_sub3$t_vec,X,type="l")
+# matplot(comb_sub3$t_vec,X,type="l")
 
 opt_pars$time_beta <- rep(0,ncol(X))  ## mob-power is incorporated (param 1)
 time_args <- nlist(X,X_date=comb_sub3$date)
@@ -66,6 +72,7 @@ run_sim_loglin(params=params, extra_pars=opt_pars["time_beta"] ## n.b. single br
 t_ont_cal_fac <- system.time(ont_cal_fac <-
                                  calibrate(data=ont_noICU
                                          , use_DEoptim=use_DEoptim
+                                 		  , DE_cores = 1
                                          , debug_plot = TRUE
                                          , base_params=params
                                          , opt_pars = opt_pars
@@ -73,3 +80,17 @@ t_ont_cal_fac <- system.time(ont_cal_fac <-
                                          , sim_fun = run_sim_loglin
                                            )
                                  ) ## system.time
+
+res <- list(mod = x, time = t_ont_cal_fac, fit = ont_cal_fac, data=ont_noICU)
+}
+
+factorial_combos <- c("0001", "0010", "0100"
+	, "0011", "0110", "0101"
+	, "0111"
+	, "1001", "1010", "1100"
+	, "1011", "1110", "1101"
+	, "1111"
+)
+res_list <- lapply(factorial_combos, function(x){run_cali(x)})
+
+#rdsave(res_list, factorial_combos)
