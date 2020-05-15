ce752761eca4d6f3b525363e30e09231589a9be6
Ben Bolker <bbolker@gmail.com>
2020-05-15 11:44:58 -0400

    tweaking factorial

--- ontario/ont_cal_factorial.R
+++ ontario/ont_cal_factorial.R
@@ -42,57 +42,71 @@ opt_pars <- list(
 )
 
 
-run_cali <- function(x){
+run_cali <- function(flags) {
 
-print(x)
+    print(flags)
 
-## flags
-use_DEoptim <-as.numeric(substr(x,1,1))
-use_mobility <- as.numeric(substr(x,2,2))
-use_spline <- as.numeric(substr(x,3,3))
-use_zeta <- as.numeric(substr(x,4,4))
+    ss <- function(flags,i) as.logical(as.numeric(substr(flags,i,i)))
+    ## flags
+    use_DEoptim <-ss(flags,1)
+    use_mobility <- ss(flags,2)
+    use_spline <- ss(flags,3)
+    use_zeta <- ss(flags,4)
 
-## now modify opt_pars according to flags
-if (use_zeta) opt_pars$params <- c(opt_pars$params,log_zeta=-1)
-loglin_terms <- "-1"
-if (use_mobility) loglin_terms <- c(loglin_terms, "log(rel_activity)")
-if (use_spline) loglin_terms <- c(loglin_terms, "bs(t_vec,df=round(length(t_vec)/14))")
-form <- reformulate(loglin_terms)
-X <- model.matrix(form, data = comb_sub3)
+    ## now modify opt_pars according to flags
+    if (use_zeta) opt_pars$params <- c(opt_pars$params,log_zeta=1)
+    loglin_terms <- "-1"
+    if (use_mobility) loglin_terms <- c(loglin_terms, "log(rel_activity)")
+    if (use_spline) loglin_terms <- c(loglin_terms, "bs(t_vec,df=round(length(t_vec)/14))")
+    form <- reformulate(loglin_terms)
+    X <- model.matrix(form, data = comb_sub3)
 
-# matplot(comb_sub3$t_vec,X,type="l")
+    matplot(comb_sub3$t_vec,X,type="l",lwd=2)
+    opt_pars$time_beta <- rep(0,ncol(X))  ## mob-power is incorporated (param 1)
+    time_args <- nlist(X,X_date=comb_sub3$date)
 
-opt_pars$time_beta <- rep(0,ncol(X))  ## mob-power is incorporated (param 1)
-time_args <- nlist(X,X_date=comb_sub3$date)
+    r0 <- run_sim_loglin(params=params, extra_pars=opt_pars["time_beta"] ## n.b. single brackets here are on purpose
+                 , time_args=time_args
+                 , sim_args=list(start_date=min(ont_all_sub$date)-15
+                               , end_date=max(ont_all_sub$date)))
 
-run_sim_loglin(params=params, extra_pars=opt_pars["time_beta"] ## n.b. single brackets here are on purpose
-             , time_args=time_args
-             , sim_args=list(start_date=min(ont_all_sub$date)-15
-                           , end_date=max(ont_all_sub$date)))
-
-## do the calibration
-t_ont_cal_fac <- system.time(ont_cal_fac <-
-                                 calibrate(data=ont_noICU
-                                         , use_DEoptim=use_DEoptim
-                                 		  , DE_cores = 1
-                                         , debug_plot = TRUE
-                                         , base_params=params
-                                         , opt_pars = opt_pars
-                                         , time_args=time_args
-                                         , sim_fun = run_sim_loglin
-                                           )
+    plot(r0,break_dates=NULL,log=TRUE)
+    ## do the calibration
+    t_ont_cal_fac <- system.time(ont_cal_fac <-
+                                     calibrate(data=ont_noICU
+                                             , use_DEoptim=use_DEoptim
+                                             , DE_cores = 1
+                                             , debug_plot = TRUE
+                                              , debug = TRUE
+                                             , base_params=params
+                                             , opt_pars = opt_pars
+                                             , time_args=time_args
+                                             , sim_fun = run_sim_loglin
+                                               )
                                  ) ## system.time
 
-res <- list(mod = x, time = t_ont_cal_fac, fit = ont_cal_fac, data=ont_noICU)
+    res <- list(mod = flags, time = t_ont_cal_fac, fit = ont_cal_fac, data=ont_noICU)
+}
+
+
+## testing
+if (FALSE) {
+    r1 <- run_cali("0001")
+    plot(r1$fit, data=ont_noICU)
+    r2 <- run_cali("0010")
+    plot(r2$fit,data=r2$data)
+    pp <- predict(r2$fit)
+    plot(pp)
+    plot(r2$fit, data=ont_noICU)
 }
 
 factorial_combos <- c("0001", "0010", "0100"
-	, "0011", "0110", "0101"
-	, "0111"
-	# , "1001", "1010", "1100"  ## Failed at 1100
-	# , "1011", "1110", "1101"
-	# , "1111"
-)
-res_list <- mclapply(factorial_combos, function(x){run_cali(x)},mc.cores=3)
+                    , "0011", "0110", "0101"
+                    , "0111"
+                                        # , "1001", "1010", "1100"  ## Failed at 1100
+                                        # , "1011", "1110", "1101"
+                                        # , "1111"
+                      )
+res_list <- mclapply(factorial_combos, run_cali, mc.cores=3)
 
 #rdsave(res_list, factorial_combos)
