49be3429f6d54dc8b958ce093abe534f6714c038
Ben Bolker <bbolker@gmail.com>
2020-05-19 21:08:09 -0400

    knot stuff

--- ontario/ont_cal_factorial.R
+++ ontario/ont_cal_factorial.R
@@ -3,9 +3,7 @@ library(splines)
 library(dplyr)
 library(parallel)
 
-## SHORTCUT for debugging
-## mle2_control <- list(maxit = 500)
-mle2_control <- list(maxit=10000)
+## FIXME: check consistency whether using mobility time series or full time series for spline basis ... ?
 
 load("ontario_clean.RData")
 load("ont_cal.RData")  ## baseline calibration: ont_cal1 (calibrated object), bd (breakpoint dates)
@@ -21,8 +19,11 @@ comb_sub2 <- (comb_sub
 print(unique(ont_all_sub$var))
 ont_noICU <- dplyr::filter(ont_all_sub, var != "ICU")
 
-X_date <- unique(ont_noICU$date)
-comb_sub3 <- (full_join(unique(select(ont_noICU,date)),comb_sub2,by="date")
+dat <- ont_noICU
+X_date <- unique(dat$date)
+
+## FIXME: should use full data set if not using mobility!
+comb_sub3 <- (full_join(unique(select(dat,date)),comb_sub2,by="date")
     ## assume missing data = constant at last value
     %>% mutate_at("rel_activity",zoo::na.locf)
     %>% mutate(t_vec=as.numeric(date-min(date)))
@@ -45,9 +46,9 @@ opt_pars <- list(
 )
 
 
-run_cali <- function(flags) {
+run_cali <- function(flags, spline_days=14, knot_quantile_var=NULL, maxit=10000) {
 
-    print(flags)
+    cat(flags, spline_days, knot_quantile_var,"\n",sep="\n")
 
     ss <- function(flags,i) as.logical(as.numeric(substr(flags,i,i)))
     ## flags
@@ -60,7 +61,21 @@ run_cali <- function(flags) {
     if (use_zeta) opt_pars$params <- c(opt_pars$params,log_zeta=1)
     loglin_terms <- "-1"
     if (use_mobility) loglin_terms <- c(loglin_terms, "log(rel_activity)")
-    if (use_spline) loglin_terms <- c(loglin_terms, "bs(t_vec,df=round(length(t_vec)/14))")
+    if (use_spline) {
+        spline_df <- round(length(comb_sub3$t_vec)/spline_days)
+        if (length(knot_quantile_var)==0) {
+            spline_term <- sprintf("bs(t_vec,df=spline_df)")
+        } else {
+            ## df specified: pick (df-degree) knots
+            tmp_dat <- na.omit(filter(dat,var==knot_quantile_var)) %>% mutate(cum=cumsum(value),t_vec=as.numeric(date-min(date)))
+            q_vec <- quantile(tmp_dat$cum,seq(1,spline_df)/(spline_df+1))
+            ## find closest dates to times with these cum sums
+            knot_t_vec <- with(tmp_dat,approx(cum,t_vec,xout=q_vec,ties=mean))$y
+            cat("knots:",knot_t_vec,sep="\n","\n")
+            spline_term <- sprintf("bs(t_vec,knots=knot_t_vec)")
+        }
+        loglin_terms <- c(loglin_terms, spline_term)
+    }
     form <- reformulate(loglin_terms)
     X <- model.matrix(form, data = comb_sub3)
 
@@ -79,13 +94,13 @@ run_cali <- function(flags) {
     debug <- !use_DEoptim
 
     t_ont_cal_fac <- system.time(ont_cal_fac <-
-                                     calibrate(data=ont_noICU
+                                     calibrate(data=dat
                                              , use_DEoptim=use_DEoptim
                                              , DE_cores = 1
                                              , debug_plot = interactive()
                                              , debug = debug
                                              , base_params=params
-                                             , mle2_control = mle2_control
+                                             , mle2_control = list(maxit=maxit)
                                              , opt_pars = opt_pars
                                              , time_args=time_args
                                              , sim_fun = run_sim_loglin
@@ -93,69 +108,13 @@ run_cali <- function(flags) {
                                  ) ## system.time
 
     
-    res <- list(mod = flags, time = t_ont_cal_fac, fit = ont_cal_fac, data=ont_noICU)
+    res <- list(mod = flags, time = t_ont_cal_fac, fit = ont_cal_fac, data=dat)
     ## checkpoint
     saveRDS(res,file=sprintf("ont_fac_%s_fit.rds",flags))
     return(res)  ## return after saving!!!!
 }
 
-
-## testing
-if (FALSE) {
-    r1 <- run_cali("1001")
-    r2 <- run_cali("0001")
-    library(bbmle)
-    r1 <- run_cali("0001")
-    plot(r1$fit, data=ont_noICU)
-    r2 <- run_cali("0010")
-    saveRDS(r2,file="r2_tmp.rds")
-    r2H <- r2
-    names(r2H$fit$forecast_args$opt_pars)
-    r2H$fit$forecast_args$opt_pars$log_nb_disp <- NULL
-    r2H$fit$forecast_args$opt_pars
-    plot(r2$fit,data=r2$data)
-    plot(r2H$fit,data=r2$data)
-    p <- coef(r2$fit$mle2)
-    invlink_trans(restore(p,r2$fit$forecast_args$opt_pars))
-    invlink_trans(restore(p,r2H$fit$forecast_args$opt_pars))
-    coef(r2$fit$mle2)
-    opt_pars2 <- opt_pars
-    opt_pars2$time_beta <- rep(0,ncol(X))  ## mob-power is incorporated (param 1)
-    ## what is different e.g. between last call to 
-    f_args <- r2$fit$forecast_args
-    f_args2 <- readRDS(".mle_checkpoint.rds")
-    setdiff(names(f_args), names(f_args2))
-    setdiff(names(f_args2), names(f_args))
-    nm <- intersect(names(f_args),names(f_args2))
-    setNames(lapply(nm,function(n)all.equal(f_args[[n]], f_args2[[n]])), nm)
-    ff <- (do.call(forecast_sim, f_args2)
-        %>% filter(var %in% McMasterPandemic:::keep_vars)
-    )
-    ff_pred <- (do.call(forecast_sim,
-                        c(list(p=f_args2$p)
-                               ## coef(r2$fit$mle2))
-                             , f_args))
-        %>% filter(var %in% McMasterPandemic:::keep_vars)
-    )
-    f_args3 <- f_args
-    f_args3$opt_pars <- f_args2$opt_pars
-    f_args3$p <- f_args2$p
-    ff_hybrid <- (do.call(forecast_sim, f_args3)
-        %>% filter(var %in% McMasterPandemic:::keep_vars)
-    )
-    
-    print(ggplot(bind_rows(forecast=ff,pred=ff_pred,hybrid=ff_hybrid,.id="source"),
-           aes(date,value,colour=var,lty=source,shape=source))
-          + geom_line()
-          + geom_point()
-          + scale_y_log10()
-          + facet_wrap(~source)
-          )
-    pp <- predict(r2$fit)
-    r2fix <- r2
-    r2fix$fit$forecast_args$opt_pars <- f_args2$opt_pars
-    plot(r2fix$fit, data=ont_noICU)
-}
+r <- run_cali("0010",knot_quantile_var="report", maxit=50)
 
 factorial_combos <- apply(expand.grid(replicate(4,0:1,simplify=FALSE)),
                 1,paste,collapse="")
