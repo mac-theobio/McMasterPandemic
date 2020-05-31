51b46db79c828c225b6d6c1b7a94401d8cd27303
wzmli <wzmichael.li@gmail.com>
2020-05-31 17:37:16 -0400

    forgot t_vec

--- ontario/ont_single_var.R
+++ ontario/ont_single_var.R
@@ -8,13 +8,13 @@ print(unique(ont_all_sub$var))
 ont_noICU <- dplyr::filter(ont_all_sub, var != "ICU")
 params <- fix_pars(read_params("ICU1.csv"))
 dat <- ont_noICU
-datH <- dat %>% filter(var == "H")
-datreport <- dat %>% filter(var == "report")
+datH <- dat %>% filter(var == "H") %>% mutate(t_vec = as.numeric(date-min(date)))
+datreport <- dat %>% filter(var == "report") %>% mutate(t_vec = as.numeric(date-min(date)))
 
 
 
-Honly <- do.call(calibrate_comb,c(nlist(params, data=datH, use_spline=TRUE, use_phenomhet=TRUE,DE_optim=FALSE)))
-reportonly <- do.call(calibrate_comb,c(nlist(params, data=datH, use_spline=TRUE, use_phenomhet=TRUE,DE_optim=FALSE)))
+Honly <- do.call(calibrate_comb,c(nlist(params, data=datH, use_spline=TRUE, use_phenomhet=TRUE,use_DEoptim=FALSE)))
+reportonly <- do.call(calibrate_comb,c(nlist(params, data=datreport, use_spline=TRUE, use_phenomhet=TRUE,use_DEoptim=FALSE)))
 
 plot(Honly)
 plot(reportonly)
