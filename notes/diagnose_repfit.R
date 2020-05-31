41a83f00dfe3a1b4864a2756dbfe5d1a9dc57333
Ben Bolker <bbolker@gmail.com>
2020-05-31 19:03:14 -0400

    update

--- notes/diagnose_repfit.R
+++ notes/diagnose_repfit.R
@@ -11,6 +11,11 @@ r1 <- run_sim(p2, stoch=c(obs=TRUE, proc=TRUE), end_date="2020-05-31")
 plot(r1, log=TRUE)
 dd <- r1 %>% select(date,report) %>% pivot() %>% na.omit()
 
+dd_rh <- r1 %>% select(date,report,hosp) %>% pivot() %>% na.omit()
+
 c1 <- calibrate_comb(params=p2, use_phenomhet=FALSE, debug_plot=TRUE, data=dd, use_DEoptim=FALSE,
+                    use_spline=FALSE)
+
+c2 <- calibrate_comb(params=p2, use_phenomhet=FALSE, debug_plot=TRUE, data=dd_rh, use_DEoptim=FALSE,
                     use_spline=FALSE)
 
