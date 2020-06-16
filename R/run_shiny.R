c8f5cbd9208b0ad42f8db0ad50ff78416874d527
Ben Bolker <bbolker@gmail.com>
2020-06-16 15:01:15 -0400

    add hcl import for shiny

--- R/run_shiny.R
+++ R/run_shiny.R
@@ -8,7 +8,7 @@
 ##'
 color_list <- function(n) {
   hues = seq(15, 375, length = n + 1)
-  colours <- hcl(h = hues, l = 65, c = 100)[1:n]
+  colours <- grDevices::hcl(h = hues, l = 65, c = 100)[1:n]
   return(colours)
 }
 
