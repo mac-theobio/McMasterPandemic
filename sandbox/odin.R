0db88b6ca76b3088b345f7c5a577a6f1530daf59
Ben Bolker <bbolker@gmail.com>
2020-06-26 19:36:36 -0400

    move epigrowthfit TMB to the package

---
+++ sandbox/odin.R
@@ -0,0 +1,20 @@
+library(McMasterPandemic)
+pars <- read_params("ICU1.csv")
+state <- make_state(param=pars)
+M <- make_ratemat(state, pars)
+odin_gradfun <- odin::odin({
+    s_init[] <- user()
+    initial(s) <- s_init
+    M[,] <- user()
+    beta0 <- user()
+    Ca <- user()
+    Cp <- user()
+    iso_m <- user()
+    Cm <- user()
+    iso_s <- user()
+    Cs <- user()
+    N <- user()
+    M[2,1] <- beta0/N*(Ca*s[3]+Cp*s[4]+(1-iso_m)*Cm*s[5]+(1-iso_s)*Cs*s[6])
+    flows <- M[i,j]*s[j]
+    deriv(s) <- sum(M[i,])-sum(M[,j])
+})
