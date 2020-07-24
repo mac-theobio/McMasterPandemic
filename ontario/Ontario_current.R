75afa77e3275e6b1808c51b7b7be19103c9999e9
wzmli <wzmichael.li@gmail.com>
2020-07-24 01:45:58 -0400

    pushing current model

--- ontario/Ontario_current.R
+++ ontario/Ontario_current.R
@@ -33,7 +33,32 @@ current <- do.call(calibrate_comb
 	)
 )
 
+current_ode <- do.call(calibrate_comb
+	, c(nlist(params=params
+		, debug_plot=FALSE
+		, sim_args = list(use_ode = TRUE)
+     	, data = trim_dat
+     	, mob_data = clean_mobility_cap
+     	, opt_pars = opt_pars
+     	, use_DEoptim = TRUE
+		, DE_cores = 7
+		, use_phenomhet = TRUE
+		, use_mobility = TRUE
+		, mob_breaks = "2020-04-15"
+		, mob_breaks_int = TRUE
+		, mob_logist_scale = 3
+		, use_spline = FALSE
+		, spline_type = "ns"
+		, spline_df = NA
+		, spline_days = 14
+		)
+	)
+)
+
 print(plot(current, data=calibrate_data_fill) + ggtitle("Current model: PH + mobility cap"))
+
+print(plot(current_ode, data=calibrate_data_fill) + ggtitle("Current model: PH + mobility cap ode"))
+
 
 saveEnvironment()
 
