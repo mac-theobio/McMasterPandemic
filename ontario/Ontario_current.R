62c6f68b18cc8421c0d65ad00771ca62da1ab0cb
wzmli <wzmichael.li@gmail.com>
2020-07-26 01:22:09 -0400

    moving everything to ndf=2

--- ontario/Ontario_current.R
+++ ontario/Ontario_current.R
@@ -11,33 +11,10 @@ clean_mobility_cap <- (clean_mobility
 )
 
 
-# current <- do.call(calibrate_comb
-# 	, c(nlist(params=params
-# 		, debug_plot=FALSE
-# 		, sim_args = list(ndt = 4)
-# 		, mle2_args = list(skip.hessian=TRUE)
-#      	, data = trim_dat
-#      	, mob_data = clean_mobility_cap
-#      	, opt_pars = opt_pars
-#      	, use_DEoptim = TRUE
-# 		, DE_cores = 7
-# 		, use_phenomhet = TRUE
-# 		, use_mobility = TRUE
-# 		, mob_breaks = "2020-04-15"
-# 		, mob_breaks_int = TRUE
-# 		, mob_logist_scale = 3
-# 		, use_spline = FALSE
-# 		, spline_type = "ns"
-# 		, spline_df = NA
-# 		, spline_days = 14
-# 		)
-# 	)
-# )
-
-current_ode <- do.call(calibrate_comb
+current <- do.call(calibrate_comb
 	, c(nlist(params=params
 		, debug_plot=FALSE
-		, sim_args = list(use_ode = TRUE)
+		, sim_args = list(ndt = 2)
 		, mle2_args = list(skip.hessian=TRUE)
      	, data = trim_dat
      	, mob_data = clean_mobility_cap
@@ -57,9 +34,7 @@ current_ode <- do.call(calibrate_comb
 	)
 )
 
-# print(plot(current, data=calibrate_data_fill) + ggtitle("Current model: PH + mobility cap"))
-
-print(plot(current_ode, data=calibrate_data_fill) + ggtitle("Current model: PH + mobility cap ode"))
+print(plot(current, data=calibrate_data_fill) + ggtitle("Current model: PH + mobility cap"))
 
 
 saveEnvironment()
--- ontario/Ontario_splines.R
+++ ontario/Ontario_splines.R
@@ -7,56 +7,10 @@ commandFiles()
 
 trim_dat <- calibrate_data_fill %>% filter(date < as.Date("2020-07-01"))
 
-# NS_splines <- do.call(calibrate_comb
-# 	, c(nlist(params=params
-# 		, debug_plot=FALSE
-# 		, sim_args = list(ndt = 4)
-# 		, mle2_args = list(skip.hessian=TRUE)
-#      	, data = trim_dat
-#      	, mob_data = clean_mobility
-#      	, opt_pars = opt_pars
-#      	, use_DEoptim = FALSE
-# 		, DE_cores = 7
-# 		, use_phenomhet = FALSE
-# 		, use_mobility = FALSE
-# 		, mob_breaks = "2020-04-15"
-# 		, mob_breaks_int = TRUE
-# 		, mob_logist_scale = 3
-# 		, use_spline = TRUE
-# 		, spline_type = "ns"
-# 		, spline_df = NA
-# 		, spline_days = 14
-# 		)
-# 	)
-# )
-# 
-# BS_splines <- do.call(calibrate_comb
-# 	, c(nlist(params=params
-# 		, debug_plot=FALSE
-# 		, sim_args = list(ndt = 4)
-# 		, mle2_args = list(skip.hessian=TRUE)
-#      	, data = trim_dat
-#      	, mob_data = clean_mobility
-#      	, opt_pars = opt_pars
-#      	, use_DEoptim = TRUE
-# 		, DE_cores = 7
-# 		, use_phenomhet = FALSE
-# 		, use_mobility = FALSE
-# 		, mob_breaks = "2020-04-15"
-# 		, mob_breaks_int = TRUE
-# 		, mob_logist_scale = 3
-# 		, use_spline = TRUE
-# 		, spline_type = "bs"
-# 		, spline_df = NA
-# 		, spline_days = 14
-# )
-# )
-# )
-
-NS_splines_ode <- do.call(calibrate_comb
+NS_splines <- do.call(calibrate_comb
 	, c(nlist(params=params
 		, debug_plot=FALSE
-		, sim_args = list(use_ode = TRUE)
+		, sim_args = list(ndt = 2)
 		, mle2_args = list(skip.hessian=TRUE)
      	, data = trim_dat
      	, mob_data = clean_mobility
@@ -76,10 +30,10 @@ NS_splines_ode <- do.call(calibrate_comb
 	)
 )
 
-BS_splines_ode <- do.call(calibrate_comb
+BS_splines <- do.call(calibrate_comb
 	, c(nlist(params=params
 		, debug_plot=FALSE
-		, sim_args = list(use_ode = TRUE)
+		, sim_args = list(ndt = 2)
 		, mle2_args = list(skip.hessian=TRUE)
      	, data = trim_dat
      	, mob_data = clean_mobility
@@ -99,13 +53,9 @@ BS_splines_ode <- do.call(calibrate_comb
 )
 )
 
-# print(plot(NS_splines, data=calibrate_data_fill) + ggtitle("NS splines"))
-# 
-# print(plot(BS_splines, data=calibrate_data_fill) + ggtitle("BS splines"))
+print(plot(NS_splines, data=calibrate_data_fill) + ggtitle("NS splines"))
 
-print(plot(NS_splines_ode, data=calibrate_data_fill) + ggtitle("NS splines ode"))
-
-print(plot(BS_splines_ode, data=calibrate_data_fill) + ggtitle("BS splines ode"))
+print(plot(BS_splines, data=calibrate_data_fill) + ggtitle("BS splines"))
 
 saveEnvironment()
 
