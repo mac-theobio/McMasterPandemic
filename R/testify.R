30f9060c2598b1cbf52c3cd1f50406220bfdd746
wzmli <wzmichael.li@gmail.com>
2020-08-02 16:16:17 -0400

    fixed unexpanded states, need to finish updating test [skip ci]

--- R/testify.R
+++ R/testify.R
@@ -10,7 +10,7 @@
 ##' pp <- read_params("PHAC_testify.csv")
 ##' state1 <- state0 <- make_state(params=pp)   ## unexpanded
 ##' state1[] <- 1  ## occupy all states
-##' state <- expand_stateval(state0)
+##' state <- val(state0)
 ##' wtsvec <- make_test_wtsvec(pp)
 ##' posvec <- make_test_posvec(pp)
 ##' ## need to make_ratemate() with *unexpanded* state, then
@@ -30,8 +30,7 @@
 ##' @export
 make_test_wtsvec <- function(params) {
     ## FIXME: why is this hard coded rather than pasted?
-    wtscats <- c("WS","WE","WIa","WIp","WIm","WIs","WH","WH2","Whosp","WICUs","WICUd","WD","WR","WX")
-    wts_vec <- with(as.list(params),setNames(c(WS,WE,WIa,WIp,WIm,WIs,WH,WH2,Whosp,WICUs,WICUd,WD,WR,WX),wtscats))
+	 wts_vec <- params[grepl("W",names(params))]
     return(wts_vec)
 }
 
@@ -46,16 +45,15 @@ update_testflows <- function(state, params, wts_vec) {
 ##' @export
 make_test_posvec <- function(params) {
     ## FIXME: why is this hard coded rather than pasted?
-    poscats <- c("PS","PE","PIa","PIp","PIm","PIs","PH","PH2","Phosp","PICUs","PICUd","PD","PR","PX")
-    pos_vec <- with(as.list(params),setNames(c(PS,PE,PIa,PIp,PIm,PIs,PH,PH2,Phosp,PICUs,PICUd,PD,PR,PX),poscats))
-    return(pos_vec)
+   pos_vec <- params[grepl("P",names(params))]
+	return(pos_vec)
 }
 
-## use inside testify to expand states
-## DRY: use this inside expand_stateval??
-expand_states <- function(x) {
-    new_states <- c(paste0(x,c("_u","_p","_n","_t")), "N", "P")
-}
+# ## use inside testify to expand states
+# ## DRY: use this inside expand_stateval??
+# expand_states <- function(expandable,nonexpandable) {
+#     new_states <- c(paste0(expandable,c("_u","_p","_n","_t")),nonexpandable, "N", "P")
+# }
 
 ##' expand states and values to include
 ##'
@@ -65,10 +63,11 @@ expand_states <- function(x) {
 ##' 
 ##' @export
 expand_stateval <- function(x, method=c("untested","spread"),
-                            add_accum=TRUE) {
+                            add_accum=TRUE,
+									 non_expanded = c("R","D","X")){
     method <- match.arg(method)
     extensions <- c("u","p","n","t")
-    newnames <- unlist(lapply(names(x), paste, extensions, sep="_"))
+    newnames <- unlist(lapply(setdiff(names(x),non_expanded), paste, extensions, sep="_"))
     new_states <- rep(0,length(newnames))
     names(new_states) <- newnames
     if (method=="untested") {
@@ -79,7 +78,8 @@ expand_stateval <- function(x, method=c("untested","spread"),
         new_states <- rep(x, each=n_expand)/n_expand
     }
     if (add_accum) {
-        new_states <- c(new_states, c(N=0,P=0))
+    ## FIXME, compare setNames = 0 code 
+        new_states <- c(new_states, setNames(numeric(length(non_expanded)), non_expanded), c(N=0,P=0))
     }
     return(new_states)
 }
@@ -102,20 +102,21 @@ testify <- function(ratemat,params,debug=FALSE){
     M <- ratemat  ## FIXME: why two names?
     states <- rownames(ratemat)
     ## testifiable vars will get expanded
+    ## We need to change expand_set here!! 
 
    ## Following page 8 L137 in McMasterReport2020-07-06.pdf 
     ## expand_set <- c("S","E","Ia","Ip","Im","Is","H","H2","hosp","ICUs","ICUd","D","R","X")
+    expand_set <- setdiff(states, c("D","R","X")) 
+    ## If these are not expandable, we need to get rid of WD, WR, WX, PD, PR, PX
+    non_expand_set <- c("D","R","X")
     ## non_expand_set <- c("P","N") ## adding P and N for accumulation
-    dummy_states <- setNames(numeric(length(states)), states)
+    dummy_states <- setNames(numeric(length(expand_set)), expand_set)
     new_states <- names(expand_stateval(dummy_states))
     
    ns <- length(new_states)
    new_M <- matrix(0,nrow=ns, ncol=ns
                  , dimnames=list(from=new_states,to=new_states)
                    )
-
-    expand_set <- setdiff(states, c("D","R","X"))
-	
    ## Between states
     for(i in rownames(ratemat)){
         ## Between states: horizontal arrows moves the same rate
@@ -129,12 +130,19 @@ testify <- function(ratemat,params,debug=FALSE){
                 new_M[paste0(i,"_p"),paste0(j,"_p")] <- M[i,j] 
                 new_M[paste0(i,"_n"),paste0(j,"_n")] <- M[i,j]
             }
+        		if((i %in% expand_set) && (j %in% non_expand_set)){
+        			 new_M[paste0(i,"_u"),j] <- M[i,j]
+        			 new_M[paste0(i,"_t"),j] <- M[i,j]
+        			 new_M[paste0(i,"_p"),j] <- M[i,j]
+        			 new_M[paste0(i,"_n"),j] <- M[i,j]
+        		}
         }
-	new_M[paste0(i,"_u"),paste0(i,"_p")] <- wtsvec[paste0("W",i)]*(posvec[paste0("P",i)])
-	new_M[paste0(i,"_u"),paste0(i,"_n")] <- wtsvec[paste0("W",i)]*(1-posvec[paste0("P",i)])
-	new_M[paste0(i,"_n"),paste0(i,"_u")] <- new_M[paste0(i,"_n"),"N"] <- omega
-	new_M[paste0(i,"_p"),paste0(i,"_t")] <- new_M[paste0(i,"_p"),"P"] <- omega
-	
+   	if(i %in% expand_set){
+			new_M[paste0(i,"_u"),paste0(i,"_p")] <- wtsvec[paste0("W",i)]*(posvec[paste0("P",i)])
+			new_M[paste0(i,"_u"),paste0(i,"_n")] <- wtsvec[paste0("W",i)]*(1-posvec[paste0("P",i)])
+			new_M[paste0(i,"_n"),paste0(i,"_u")] <- new_M[paste0(i,"_n"),"N"] <- omega
+			new_M[paste0(i,"_p"),paste0(i,"_t")] <- new_M[paste0(i,"_p"),"P"] <- omega
+   	}
     }
     return(new_M)
 }
