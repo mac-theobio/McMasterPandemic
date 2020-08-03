79cf3029cd5f64ff0e21e661cdd817252acf1311
Jonathan Dushoff <dushoff@mcmaster.ca>
2020-08-03 14:16:15 -0400

    chesterfield

--- notes/chesterfield.R
+++ notes/chesterfield.R
@@ -1,6 +1,10 @@
+options(stringsAsFactors=FALSE)
 library(McMasterPandemic)
 load(system.file("testdata","Ontario_basic.rda",package="McMasterPandemic"))
+options(stringsAsFactors=FALSE)
 test_mle_pred <- predict(Ontario_fit)
+
+all.equal(test_mle_pred, mle_prediction)
 
 str(test_mle_pred)
 
