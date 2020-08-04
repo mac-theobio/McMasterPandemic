46b04f369601708963632ca6d4fc3388866709fe
Jonathan Dushoff <dushoff@mcmaster.ca>
2020-08-03 20:54:20 -0400

    Working with Ali

--- notes/rshape.R
+++ notes/rshape.R
@@ -6,8 +6,10 @@ source("makestuff/makeRfuns.R")
 
 makeGraphics()
 
-fitmax <- 98 #98
-t <- 1:122 #it was 140
+tmax <- 112
+fitmax <- 98
+
+t <- 1:tmax #it was 140
 r <- 0.04
 h <- 2
 R0 <- 3
@@ -16,50 +18,31 @@ c <- 2
 Q <- exp(r*t)
 S <- 1/(1+Q)
 I <- Q/(1+Q^2)
-
 Rt <- R0*S^h*I^(-c)
 
 Rf <- data.frame(
-	t=t, Rt=Rt
-)
-
-Rf <- (Rf
-	%>% mutate (Rt=ifelse(t>fitmax, NA, Rt))
+	t=t
+	, Rt=ifelse(t>fitmax, NA, Rt)
 )
 
 print(Rf)
 
-
-
-
-
 mod <- lm(Rt~bs(t,df=7),data=Rf)
-cmod <- coef(mod) 
-print(cmod)
-
-t_sub <- t[t %in% c(1:fitmax)]
+co <- coef(mod) 
+print(co)
 
 Rf <- (Rf
-       %>% mutate (Rt_predict=ifelse(t>fitmax, NA, predict(mod, data=t_sub ) ))
+	%>% mutate(Rt_predict = predict(mod, newdata=Rf))
 )
-
-pts <- (ggplot(Rf)
-        + aes(t, Rt)
-        + geom_point()
-        +geom_line(aes(t,Rt_predict), col="red")
-        + xlim(range(t))
-        + ylim(c(0, NA))
-)
-print(pts) 
- 
 
 #to construct the Rt from lm:
 b <- bs(t,df=7)
-length(b[,1])
-plot(t, as.matrix(cbind(rep(1,length(t)) ,b[,])) %*% matrix(cmod,ncol=1))
+b <- as.matrix(cbind(rep(1, nrow(b)), b[,]))
+aliP <- b %*% matrix(co,ncol=1)
 
-# no intercept
-strmod_nointercept <- update(mod, .~.-1)  
-print(coef(mod_nointercept) )
-
-
+print(ggplot(Rf)
+	+ aes(t, Rt)
+	+ geom_line()
+	+ geom_point(aes(t,Rt_predict), col="red", shape=1)
+	+ geom_point(aes(t, aliP), col="blue", shape=3)
+)
