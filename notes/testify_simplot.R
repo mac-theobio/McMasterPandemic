95411c4c8d083adff84fe88ce155adb983deac06
wzmli <wzmichael.li@gmail.com>
2020-08-06 19:45:57 -0400

    not understanding positive test

--- notes/testify_simplot.R
+++ notes/testify_simplot.R
@@ -5,18 +5,16 @@ library(cowplot)
 source("makestuff/makeRfuns.R")
 commandFiles()
 
-gg <- (ggplot(simcombo, aes(x=date,y=med, color=type, shape=var, group=type))
+gg <- (ggplot(simcombo, aes(x=date,y=med, color=var, group=type))
 	+ geom_point()
 	+ scale_color_manual(values=c("black","red","blue"))
-	+ scale_fill_manual(values=c("black","red","blue"))
 	+ scale_y_log10()
 	+ ylab("Daily count")
-	+ facet_wrap(~params, ncol=3)
+	+ facet_wrap(~params, nrow=3)
 	+ theme(legend.position = "bottom")
 )
 
 
 print(gg)
 
-print(gg %+% (simcombo %>% filter(var != "incidence")))
-
+print(gg %+% (simcombo %>% filter(params == "default")))
