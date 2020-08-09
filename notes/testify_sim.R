ed9f5225fbc5a3272db09e90c2b667f289181c53
Ben Bolker <bbolker@gmail.com>
2020-08-09 15:37:35 -0400

    messing around with testify simplots

--- notes/testify_sim.R
+++ notes/testify_sim.R
@@ -11,13 +11,18 @@ set.seed(0807)
 start <- as.Date("2020-01-01")
 end <- as.Date("2020-06-01")
 
-pp <- read_params(matchFile(".csv$"))
-pp[["iso_t"]] <- 0
-pp[["N"]] <- 1.5e7 ## population of Ontario
-pp[["beta0"]] <- 0.4 ## Make a semi-realistic R0
-pp[["testing_intensity"]] <- 0.2
+fn <- if (interactive()) "PHAC_testify.csv" else matchFile(".csv$")
+pp <- (read_params(fn)
+    %>% update(
+            iso_t=0         ## no isolation of tested
+            , N=1.5e7       ## population of Ontario
+            , beta0=0.4     ## Make a semi-realistic R0
+            , testing_intensity=0.2  ## high testing intensity so we can see what's going on
+        )
+)
 
 ppw0 <- pp[!grepl("^W",names(pp))] ## Copying BMB, removing all of the regular W-parameters
+class(ppw0) <- "params_pansim"
 
 print(pp)
 summary(pp)
@@ -25,35 +30,36 @@ summary(pp)
 W_asymp <- c(0.01, 0.1,1)
 iso_t <- c(0,0.5,0.9,1)
 
-simframe <- data.frame()
-for(i in W_asymp){
-	for(j in iso_t){
-		ppw0[["W_asymp"]] <- i
-		ppw0[["iso_t"]] <- j
-                sims <- (run_sim(params = ppw0, ratemat_args = list(testify=TRUE)
-                               , start_date = start
-                               , end_date = end
-                                 ##			, condense_args=list(keep_all=TRUE) checkout the expanded version
-                                 ) 
-                    %>% mutate(W_asymp = paste0("W_asymp = ",i)
-                             , iso_t = paste0("iso_t = ",j)
-                               )
-                )
-                simframe <- bind_rows(simframe,sims)
-	}
+simlist <- list()
+for(i in W_asymp) {
+    for(j in iso_t) {
+        ppw0 <- update(ppw0, W_asymp=i, iso_t = j)
+        sims <- (run_sim(params = ppw0, ratemat_args = list(testify=TRUE)
+                       , start_date = start
+                       , end_date = end
+                         ##			, condense_args=list(keep_all=TRUE) checkout the expanded version
+                         )
+            %>% mutate(W_asymp = i,
+                       iso_t = j)
+        )
+        simlist <- c(simlist,list(sims))
+    }
 }
+simframe <- bind_rows(simlist)
 
 print(simframe)
 
 simdat <- (simframe
-	%>% transmute(date,incidence,postest
-			, total_test = postest + negtest
-			, report
-			, W_asymp
-			, iso_t
-		)
-	%>% gather(key="var",value="value",-c(date, W_asymp, iso_t))
+    %>% transmute(date
+                , incidence
+                , postest
+                , total_test = postest + negtest
+                , positivity = postest/total_test
+                , report
+                , W_asymp
+                , iso_t
+                  )
+    %>% gather(key="var",value="value",-c(date, W_asymp, iso_t))
 )
-
 
 saveVars(simdat)
--- notes/testify_simplot.R
+++ notes/testify_simplot.R
@@ -1,19 +1,27 @@
-library(ggplot2);theme_set(theme_bw())
+library(ggplot2);theme_set(theme_bw()+theme(panel.spacing=grid::unit(0,"lines")))
 library(dplyr)
+library(tidyr)
 library(cowplot)
 
 source("makestuff/makeRfuns.R")
 commandFiles()
 
+## ugh: modify test positivity to percentage so visible
+simdat <- (simdat
+    %>% pivot_wider(names_from=var,values_from=value)
+    %>% mutate_at("positivity", ~ . * 100)
+    %>% pivot_longer(-c(date, W_asymp, iso_t), names_to="var")
+)
+
 gg <- (ggplot(simdat)
 	+ aes(x=date,y=value)
 	+ geom_line()
-	+ scale_color_manual(values=c("black","red","blue","orange"))
-	+ scale_y_log10(limits=c(1, NA))
+	+ scale_color_manual(values=c("black","red","blue","orange","purple"))
+	+ scale_y_log10(limits=c(0.1, NA))
 	+ ylab("Daily count")
 	+ theme(legend.position = "bottom")
 )
 
-print(gg + facet_grid(W_asymp~iso_t) + aes(color=var))
-print(gg + facet_grid(W_asymp~var) + aes(color=iso_t))
-print(gg + facet_grid(iso_t~var) + aes(color=W_asymp))
+print(gg + facet_grid(W_asymp~iso_t, labeller=label_both) + aes(color=var))
+print(gg + facet_grid(W_asymp~var, labeller=label_both) + aes(color=factor(iso_t)))
+print(gg + facet_grid(iso_t~var, labeller=label_both) + aes(color=factor(W_asymp)))
