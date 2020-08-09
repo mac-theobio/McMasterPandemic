ef7c84232c2b6352777d058cf4d0ff0c32707e73
Ben Bolker <bbolker@gmail.com>
2020-08-09 18:47:19 -0400

    fussing with testing plots

--- notes/testify_sim.R
+++ notes/testify_sim.R
@@ -12,7 +12,7 @@ start <- as.Date("2020-01-01")
 end <- as.Date("2020-06-01")
 
 fn <- if (interactive()) "PHAC_testify.csv" else matchFile(".csv$")
-pp <- (read_params(fn)
+params <- (read_params(fn)
     %>% fix_pars(target=c(R0=2.5, Gbar=6))
     %>% update(
             iso_t=0         ## no isolation of tested
@@ -21,11 +21,11 @@ pp <- (read_params(fn)
         )
 )
 
-ppw0 <- pp[!grepl("^W",names(pp))] ## Copying BMB, removing all of the regular W-parameters
-class(ppw0) <- "params_pansim"
+paramsw0 <- params[!grepl("^W",names(params))] ## Copying BMB, removing all of the regular W-parameters
+class(paramsw0) <- "params_pansim"
 
-print(ppw0)
-summary(ppw0)
+print(paramsw0)
+summary(paramsw0)
 
 testing_intensity <- c(0.002, 0.02, 0.2)
 W_asymp <- c(0.01, 0.1,1)
@@ -35,8 +35,8 @@ simlist <- list()
 for(i in W_asymp) {
     for (j in iso_t) {
         for (k in testing_intensity) {
-            ppw0 <- update(ppw0, W_asymp=i, iso_t = j, testing_intensity=k)
-            sims <- (run_sim(params = ppw0, ratemat_args = list(testify=TRUE)
+            paramsw0 <- update(paramsw0, W_asymp=i, iso_t = j, testing_intensity=k)
+            sims <- (run_sim(params = paramsw0, ratemat_args = list(testify=TRUE)
                            , start_date = start
                            , end_date = end
                              ##			, condense_args=list(keep_all=TRUE) checkout the expanded version
@@ -68,4 +68,4 @@ simdat <- (simframe
     %>% gather(key="var",value="value",-c(date, W_asymp, iso_t, testing_intensity))
 )
 
-saveVars(simdat)
+saveVars(simdat, params)
--- notes/testify_simplot.R
+++ notes/testify_simplot.R
@@ -5,9 +5,10 @@ library(cowplot)
 
 source("makestuff/makeRfuns.R")
 commandFiles()
+if (interactive()) load("testify_sim.rda")
 
 ymin <- 1    
-ymax <- 1e7  ## screen out pathological values
+ymax <- 1e6  ## screen out pathological values
 
 ## ugh: modify test positivity to percentage so visible
 simdat <- (simdat
@@ -16,6 +17,12 @@ simdat <- (simdat
     %>% pivot_longer(-c(date, W_asymp, iso_t, testing_intensity), names_to="var")
 )
 
+totaltest_data <- (tibble(
+    var="total_test",
+    testing_intensity=unique(simdat$testing_intensity))
+    %>%     mutate(value=testing_intensity*params[["N"]])
+)
+    
 gg <- (ggplot(simdat)
     + aes(x=date,y=value)
     + geom_line()
@@ -23,13 +30,18 @@ gg <- (ggplot(simdat)
     + scale_y_log10(limits=c(ymin, ymax),oob=scales::squish)
     + ylab("Daily count")
     + theme(legend.position = "bottom")
-    
 )
 
-ff <- function(i) filter(simdat, testing_intensity==i)
+
+ff <- function(i,data=simdat) filter(data, testing_intensity==i)
 mm <- function(i) ggtitle(sprintf("testing intensity=%1.2g",i))
 for (i in unique(simdat$testing_intensity)) {
-    print(gg %+% ff(i) + mm(i) + facet_grid(W_asymp~iso_t, labeller=label_both) + aes(color=var))
-    print(gg %+% ff(i) + mm(i) + facet_grid(W_asymp~var, labeller=label_both) + aes(color=factor(iso_t)))
-    print(gg %+% ff(i) + mm(i) + facet_grid(iso_t~var, labeller=label_both) + aes(color=factor(W_asymp)))
+    ggx <- (gg
+        %+% ff(i)
+        + mm(i)
+        + geom_hline(data=ff(i,totaltest_data),aes(yintercept=value),lty=2)
+    )
+    print(ggx + facet_grid(W_asymp~iso_t, labeller=label_both) + aes(color=var))
+    print(ggx + facet_grid(W_asymp~var, labeller=label_both) + aes(color=factor(iso_t)))
+    print(ggx + facet_grid(iso_t~var, labeller=label_both) + aes(color=factor(W_asymp)))
 }
