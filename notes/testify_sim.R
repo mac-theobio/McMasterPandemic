d0a0363a5107667590aff2fcba114188c6476b83
Jonathan Dushoff <dushoff@mcmaster.ca>
2020-08-09 22:43:04 -0400

    amsync

--- notes/testify_sim.R
+++ notes/testify_sim.R
@@ -11,61 +11,49 @@ set.seed(0807)
 start <- as.Date("2020-01-01")
 end <- as.Date("2020-06-01")
 
-fn <- if (interactive()) "PHAC_testify.csv" else matchFile(".csv$")
-params <- (read_params(fn)
-    %>% fix_pars(target=c(R0=2.5, Gbar=6))
-    %>% update(
-            iso_t=0         ## no isolation of tested
-            , N=1.5e7       ## population of Ontario
-            , testing_intensity=0.2  ## high testing intensity so we can see what's going on
-        )
-)
+pp <- read_params(matchFile(".csv$"))
+pp[["iso_p"]] <- 0
+pp[["N"]] <- 1.5e7 ## population of Ontario
+pp[["beta0"]] <- 0.4 ## Make a semi-realistic R0
+pp[["testing_intensity"]] <- 0.2
 
-paramsw0 <- params[!grepl("^W",names(params))] ## Copying BMB, removing all of the regular W-parameters
-class(paramsw0) <- "params_pansim"
+ppw0 <- pp[!grepl("^W",names(pp))] ## Copying BMB, removing all of the regular W-parameters
 
-print(paramsw0)
-summary(paramsw0)
+print(pp)
+summary(pp)
 
-testing_intensity <- c(0.002, 0.02, 0.2)
-W_asymp <- c(0.01, 0.1,1)
-iso_t <- c(0,0.5,0.9,1)
+Wasymp <- c(0.01, 0.1,1)
+isop <- c(0,0.5,0.9,1)
 
-simlist <- list()
-for(i in W_asymp) {
-    for (j in iso_t) {
-        for (k in testing_intensity) {
-            paramsw0 <- update(paramsw0, W_asymp=i, iso_t = j, testing_intensity=k)
-            sims <- (run_sim(params = paramsw0, ratemat_args = list(testify=TRUE)
-                           , start_date = start
-                           , end_date = end
-                             ##			, condense_args=list(keep_all=TRUE) checkout the expanded version
-                             )
-                %>% mutate(W_asymp = i
-                         , iso_t = j
-                         , testing_intensity=k
-                           )
-            )
-            simlist <- c(simlist,list(sims))
-        } ## loop over testing intensity
-    } ## loop over iso_t
-} ## loop over W_asymp
-simframe <- bind_rows(simlist)
+simframe <- data.frame()
+for(i in Wasymp){
+	for(j in isop){
+		ppw0[["W_asymp"]] <- i
+		ppw0[["iso_p"]] <- j
+			sims <- (run_sim(params = ppw0, ratemat_args = list(testify=TRUE)
+			, start_date = start
+			, end_date = end
+##			, condense_args=list(keep_all=TRUE) checkout the expanded version
+		) 
+	%>% mutate(W_asymp = paste0("W_asymp = ",i)
+			, iso_p = paste0("iso_p = ",j)
+			)
+	)
+	simframe <- bind_rows(simframe,sims)
+	}
+}
 
 print(simframe)
 
 simdat <- (simframe
-    %>% transmute(date
-                , incidence
-                , postest
-                , total_test = postest + negtest
-                , positivity = postest/total_test
-                , report
-                , W_asymp
-                , iso_t
-                , testing_intensity
-                  )
-    %>% gather(key="var",value="value",-c(date, W_asymp, iso_t, testing_intensity))
+	%>% transmute(date,incidence,postest
+			, total_test = postest + negtest
+			, report
+			, W_asymp
+			, iso_p
+		)
+	%>% gather(key="var",value="value",-date, -W_asymp, -iso_p)
 )
 
-saveVars(simdat, params)
+
+saveVars(simdat)
