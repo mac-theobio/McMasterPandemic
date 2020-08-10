f3e25cd734953b4b59c30d3fd6953912d5e5a741
Ben Bolker <bbolker@gmail.com>
2020-08-10 16:40:10 -0400

    testify sims saving category

--- notes/testify_sim.R
+++ notes/testify_sim.R
@@ -9,16 +9,19 @@ library(devtools); load_all("../")
 if (interactive()) {
 	use_ode <- FALSE
 	testwt_scale <- "none" ## or "N" or "sum_u"
-	testing_intensity <- c(0.002, 0.02, 0.2)
-	W_asymp <- c(0.01, 0.1,1)
-	iso_t <- c(0,0.5,0.9,1)
+	testing_intensity <- 0.002 ## c(0.002, 0.02, 0.2)
+	W_asymp <-  1 ## c(0.01, 0.1,1)
+	iso_t <- 0.5 ## c(0,0.5,0.9,1)
 	start <- as.Date("2020-01-01")
 	end <- as.Date("2020-06-01")
 	pop <- 1.5e7       ## population of Ontario
-   R0 <- 2.5
+        R0 <- 2.5
 	Gbar <- 6
 	set.seed(0807)
+        keep_all <- FALSE
 }
+
+if (!exists("keep_all")) keep_all <- FALSE
 
 fn <- if (interactive()) "PHAC_testify.csv" else matchFile(".csv$")
 params <- (read_params(fn)
@@ -46,7 +49,7 @@ for(i in W_asymp) {
                            , end_date = end
                            , use_ode = use_ode
                            , step_args = list(testwt_scale=testwt_scale)
-                             ##			, condense_args=list(keep_all=TRUE) checkout the expanded version
+                           , condense_args=list(keep_all=keep_all, add_reports=!keep_all) ## checkout the expanded version
                              )
                 %>% mutate(W_asymp = i
                          , iso_t = j
@@ -61,19 +64,37 @@ simframe <- bind_rows(simlist)
 
 print(simframe)
 
-simdat <- (simframe
-    %>% transmute(date
-		 , incidence
-		 , postest
-		 , total_test = postest + negtest
-		 , pos_per_million = 1e6*postest/total_test
-		 , report
-		 , W_asymp
-		 , iso_t
-		 , testing_intensity
-	)
-    %>% gather(key="var",value="value",-c(date, W_asymp, iso_t, testing_intensity))
-)
+if (!keep_all) {
+    simdat <- (simframe
+        %>% transmute(date
+                    , incidence
+                    , postest
+                    , total_test = postest + negtest
+                    , pos_per_million = 1e6*postest/total_test
+                    , report
+                    , W_asymp
+                    , iso_t
+                    , testing_intensity
+                      )
+        %>% gather(key="var",value="value",-c(date, W_asymp, iso_t, testing_intensity))
+    )
+} else {
+    
+    simdat <- (simframe
+        %>% select(-c(R,D,foi,N,P))
+        %>% gather(key="var",value="value",-c(date, W_asymp, iso_t, testing_intensity))
+        %>% separate(var,c("pref","testcat"),sep="_")
+        %>% mutate_at("pref", ~ case_when(grepl("^([Hh]|IC)",.) ~ "hosp",
+                                          grepl("^I[ap]",.) ~ "asymp_I",
+                                          TRUE ~ .)
+                      )
+        %>% group_by(date,W_asymp, iso_t, testing_intensity, pref, testcat)
+        %>% summarise(value=mean(value),.groups="drop")
+        %>% mutate_at("pref", factor, levels=c("S","E","asymp_I","Im","Is","hosp"))
+        %>% mutate_at("testcat", factor, levels=c("u","n","p","t"))
+    )
+    
+}
 
 warnings()
 
