7fd9927897e181b2560033e4bac07aff3ee90481
wzmli <wzmichael.li@gmail.com>
2020-08-10 11:58:08 -0400

    adding ode.R

---
+++ notes/ode.R
@@ -0,0 +1,8 @@
+use_ode <- TRUE
+testwt_scale <- "none" ## or "N" or "sum_u"
+testing_intensity <- c(0.002, 0.02, 0.2)
+W_asymp <- c(0.01, 0.1,1)
+iso_t <- c(0,0.5,0.9,1)
+set.seed(0807)
+start <- as.Date("2020-01-01")
+end <- as.Date("2020-06-01")
