ddc22fe136300e295079f454636e298a9fa1217f
Ben Bolker <bbolker@gmail.com>
2020-08-10 19:16:09 -0400

    add testify plots by testing category [skip ci]

---
+++ notes/testify_testcatplot.R
@@ -0,0 +1,38 @@
+library(ggplot2);theme_set(theme_bw()+theme(panel.spacing=grid::unit(0,"lines")))
+library(dplyr)
+library(tidyr)
+library(cowplot)
+library(colorspace)
+scale_colour_discrete <- colorspace::scale_colour_discrete_qualitative
+
+source("makestuff/makeRfuns.R")
+commandEnvironments()
+if (!interactive()) {
+    makeGraphics()
+} else {
+    load("testify_sim.rda")
+}
+
+ymin <- 1    
+ymax <- 1e6  ## screen out pathological values
+
+
+gg <- (ggplot(simdat)
+    + aes(x=date,y=value)
+    + geom_line(aes(linetype=testcat))
+    + scale_y_log10()
+)
+
+ff <- function(i,data=simdat) filter(data, testing_intensity==i)
+mm <- function(i) ggtitle(sprintf("testing intensity=%1.2g",i))
+
+## FIXME: now we have two more dimensions to deal with (pref or testcat), that's annoying
+for (i in unique(simdat$testing_intensity)) {
+    ggx <- (gg
+        %+% ff(i)
+        + mm(i)
+    )
+    print(ggx + facet_grid(W_asymp~iso_t, labeller=label_both) + aes(color=pref))
+    print(ggx + facet_grid(W_asymp~pref, labeller=label_both) + aes(color=factor(iso_t)))
+    print(ggx + facet_grid(iso_t~pref, labeller=label_both) + aes(color=factor(W_asymp)))
+}
