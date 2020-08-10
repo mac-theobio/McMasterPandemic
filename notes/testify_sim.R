855ff8262fe3eac4920d6fe942960249ba560c34
Jonathan Dushoff <dushoff@mcmaster.ca>
2020-08-09 22:46:50 -0400

    Apparently fix a disaster made when I tried to branch.

--- notes/testify_sim.R
+++ notes/testify_sim.R
@@ -11,49 +11,61 @@ set.seed(0807)
 start <- as.Date("2020-01-01")
 end <- as.Date("2020-06-01")
 
-pp <- read_params(matchFile(".csv$"))
-pp[["iso_p"]] <- 0
-pp[["N"]] <- 1.5e7 ## population of Ontario
-pp[["beta0"]] <- 0.4 ## Make a semi-realistic R0
-pp[["testing_intensity"]] <- 0.2
+fn <- if (interactive()) "PHAC_testify.csv" else matchFile(".csv$")
+params <- (read_params(fn)
+    %>% fix_pars(target=c(R0=2.5, Gbar=6))
+    %>% update(
+            iso_t=0         ## no isolation of tested
+            , N=1.5e7       ## population of Ontario
+            , testing_intensity=0.2  ## high testing intensity so we can see what's going on
+        )
+)
 
-ppw0 <- pp[!grepl("^W",names(pp))] ## Copying BMB, removing all of the regular W-parameters
+paramsw0 <- params[!grepl("^W",names(params))] ## Copying BMB, removing all of the regular W-parameters
+class(paramsw0) <- "params_pansim"
 
-print(pp)
-summary(pp)
+print(paramsw0)
+summary(paramsw0)
 
-Wasymp <- c(0.01, 0.1,1)
-isop <- c(0,0.5,0.9,1)
+testing_intensity <- c(0.002, 0.02, 0.2)
+W_asymp <- c(0.01, 0.1,1)
+iso_t <- c(0,0.5,0.9,1)
 
-simframe <- data.frame()
-for(i in Wasymp){
-	for(j in isop){
-		ppw0[["W_asymp"]] <- i
-		ppw0[["iso_p"]] <- j
-			sims <- (run_sim(params = ppw0, ratemat_args = list(testify=TRUE)
-			, start_date = start
-			, end_date = end
-##			, condense_args=list(keep_all=TRUE) checkout the expanded version
-		) 
-	%>% mutate(W_asymp = paste0("W_asymp = ",i)
-			, iso_p = paste0("iso_p = ",j)
-			)
-	)
-	simframe <- bind_rows(simframe,sims)
-	}
-}
+simlist <- list()
+for(i in W_asymp) {
+    for (j in iso_t) {
+        for (k in testing_intensity) {
+            paramsw0 <- update(paramsw0, W_asymp=i, iso_t = j, testing_intensity=k)
+            sims <- (run_sim(params = paramsw0, ratemat_args = list(testify=TRUE)
+                           , start_date = start
+                           , end_date = end
+                             ##			, condense_args=list(keep_all=TRUE) checkout the expanded version
+                             )
+                %>% mutate(W_asymp = i
+                         , iso_t = j
+                         , testing_intensity=k
+                           )
+            )
+            simlist <- c(simlist,list(sims))
+        } ## loop over testing intensity
+    } ## loop over iso_t
+} ## loop over W_asymp
+simframe <- bind_rows(simlist)
 
 print(simframe)
 
 simdat <- (simframe
-	%>% transmute(date,incidence,postest
-			, total_test = postest + negtest
-			, report
-			, W_asymp
-			, iso_p
-		)
-	%>% gather(key="var",value="value",-date, -W_asymp, -iso_p)
+    %>% transmute(date
+                , incidence
+                , postest
+                , total_test = postest + negtest
+                , positivity = postest/total_test
+                , report
+                , W_asymp
+                , iso_t
+                , testing_intensity
+                  )
+    %>% gather(key="var",value="value",-c(date, W_asymp, iso_t, testing_intensity))
 )
 
-
-saveVars(simdat)
+saveVars(simdat, params)
--- notes/testify_simplot.R
+++ notes/testify_simplot.R
@@ -1,19 +1,47 @@
-library(ggplot2);theme_set(theme_bw())
+library(ggplot2);theme_set(theme_bw()+theme(panel.spacing=grid::unit(0,"lines")))
 library(dplyr)
+library(tidyr)
 library(cowplot)
 
 source("makestuff/makeRfuns.R")
 commandFiles()
+if (interactive()) load("testify_sim.rda")
 
-gg <- (ggplot(simdat)
-	+ aes(x=date,y=value)
-	+ geom_line()
-	+ scale_color_manual(values=c("black","red","blue","orange"))
-	+ scale_y_log10(limits=c(1, NA))
-	+ ylab("Daily count")
-	+ theme(legend.position = "bottom")
+ymin <- 1    
+ymax <- 1e6  ## screen out pathological values
+
+## ugh: modify test positivity to percentage so visible
+simdat <- (simdat
+    %>% pivot_wider(names_from=var,values_from=value)
+    %>% mutate_at("positivity", ~ . * 100)
+    %>% pivot_longer(-c(date, W_asymp, iso_t, testing_intensity), names_to="var")
 )
 
-print(gg + facet_grid(W_asymp~iso_p) + aes(color=var))
-print(gg + facet_grid(W_asymp~var) + aes(color=iso_p))
-print(gg + facet_grid(iso_p~var) + aes(color=W_asymp))
+totaltest_data <- (tibble(
+    var="total_test",
+    testing_intensity=unique(simdat$testing_intensity))
+    %>%     mutate(value=testing_intensity*params[["N"]])
+)
+    
+gg <- (ggplot(simdat)
+    + aes(x=date,y=value)
+    + geom_line()
+    + scale_color_manual(values=c("black","red","blue","orange","purple"))
+    + scale_y_log10(limits=c(ymin, ymax),oob=scales::squish)
+    + ylab("Daily count")
+    + theme(legend.position = "bottom")
+)
+
+
+ff <- function(i,data=simdat) filter(data, testing_intensity==i)
+mm <- function(i) ggtitle(sprintf("testing intensity=%1.2g",i))
+for (i in unique(simdat$testing_intensity)) {
+    ggx <- (gg
+        %+% ff(i)
+        + mm(i)
+        + geom_hline(data=ff(i,totaltest_data),aes(yintercept=value),lty=2)
+    )
+    print(ggx + facet_grid(W_asymp~iso_t, labeller=label_both) + aes(color=var))
+    print(ggx + facet_grid(W_asymp~var, labeller=label_both) + aes(color=factor(iso_t)))
+    print(ggx + facet_grid(iso_t~var, labeller=label_both) + aes(color=factor(W_asymp)))
+}
