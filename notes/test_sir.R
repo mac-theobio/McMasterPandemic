917b3e44755763919c4503b3193944741b2394a2
aligharouni <aligharouni@gmail.com>
2020-08-11 19:44:58 -0400

    update sir model

--- notes/test_sir.R
+++ notes/test_sir.R
@@ -25,14 +25,14 @@ sir.model <- function(t,x,params){
   list(dxdt)
 }
 
+dd <- (data.frame(date=seq(as.Date("2020-02-14"),
+                           as.Date("2020-03-14"),
+                           by="1 day"))
+)
+
 params0 <- c(beta=1,gamma=1/3)
 
 xstart <- c(S=N0,I=1,R=0) ## initial conditions
-
-dd <- (data.frame(date=seq(as.Date("2020-02-14"),
-                                    as.Date("2020-05-31"),
-                                    by="1 day"))
-)
 
 
 out <- as.data.frame(
@@ -59,14 +59,36 @@ library(dplyr)
 devtools::load_all() ## update code if necessary
 
 
+my_inf <- 100 #Inf
 params <- read_params("ICU1.csv")
+attributes(params)
 # set all params to be 0 except for the corresponding params in SIR
-params[1:length(params)] <- 0  
-params["beta"] <- 1
+params["Ca"] <-1
+params[3:5] <- 0 # all relative transmissions, i.e., C_ 
+  
+params["beta0"] <- 1
+params["alpha"] <-1 #assuming that all exposed people go to I_a
+
+params["sigma"] <- my_inf #Inf
+
 params["gamma_a"] <- 1/3
+params["gamma_m"] <- my_inf
+
+params["gamma_s"] <- my_inf
+params["gamma_p"] <- my_inf
+params["rho"] <- my_inf
+
+params["delta"] <-0
+params["mu"] <-0
+
 params["N"] <- N0
 params["E0"] <- 1
 
+
+
+
+
+  
 ## run calibrate_comb() with interesting spline settings to
 ##  return the time-varying beta log-linear model matrix:
 X <- calibrate_comb(data=dd, params=params,
@@ -80,11 +102,15 @@ matplot(X, type="l",lty=1, lwd=2,
         col=palette(),
         xlab="day",ylab="basis function value")
 
+# debugonce(run_sim_loglin)
 sims <- run_sim_loglin(params=params,
-                       # extra_pars=list(time_beta=bb),
-                       time_args=list(X_date=date, X=X),
-                       sim_args=list(start_date=date[1],end_date=tail(date,1))
+                       # extra_pars= NULL,
+                       # time_args=list(X_date=dd$date, X=X),
+                       sim_args=list(start_date=dd$date[1],end_date=tail(dd$date,1))
 )
 
+out_temp <- na.omit(as.data.frame(sims[c("date","S","I","R")]))
+out3 <- out_temp %>%
+  pivot_longer(c(S, I, R), names_to = "compartment", values_to = "value")
 
-
+ggplot(data=na.omit(out3[1:25,]), aes(x=date,y=value,col=compartment))+geom_line()
