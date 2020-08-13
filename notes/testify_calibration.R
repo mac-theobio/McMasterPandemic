c7c63a9c5bb05738ede36897fb845bea41a86dad
wzmli <wzmichael.li@gmail.com>
2020-08-13 03:04:43 -0400

    testify still doesn't work nicely after change

--- notes/testify_calibration.R
+++ notes/testify_calibration.R
@@ -11,49 +11,29 @@ print(params)
 paramsw0 <- params
 summary(paramsw0)
 keep_all <- FALSE
-print(pf[1,])
 simdat <- simtestify(1)
 
-pdat <- (simdat
-	%>% gather(key="var",value="value",-c(date,W_asymp,iso_t,testing_intensity))
+
+dat <- simdat %>% transmute(date, var="postest", value=postest)
+pp <- attr(simdat,"params")
+
+opt_pars <- with(as.list(pp),
+                 list(params=c(log_E0=log(E0)
+                             , log_beta0=log(beta0)
+                             ## , logit_mu = plogis(mu)
+                             ## , logit_phi1 = plogis(phi1)
+                             , logit_W_asymp = plogis(W_asymp)
+                             , log_testing_intensity=log(0.002)
+                 )
+                 )
 )
-
-last_order <- (pdat
-	%>% filter(date == max(date))
-	%>% arrange(desc(value))
-)
-
-pdat <- (pdat %>% mutate(var = factor(var,levels=last_order$var)))
-
-gg <- (ggplot(pdat,aes(date,value))
-	+ geom_line()
-	+ facet_wrap(~var,scale="free")
-	+ scale_y_log10()
-)
-
-print(gg)
-
-quit()
-
-dat <- simdat2 %>% transmute(date, var="postest", value=postest)
-pp <- attr(simdat2,"params")
-
-## opt_pars <- with(as.list(pp),
-##                  list(params=c(log_E0=log(E0)
-##                              , log_beta0=log(beta0)
-##                              ## , logit_mu = plogis(mu)
-##                              ## , logit_phi1 = plogis(phi1)
-##                              , logit_W_asymp = plogis(W_asymp)
-##                              , log_testing_intensity=log(0.002)
                                
-opt_pars <- list(params=c(log_E0=2, log_beta0=-1, logit_mu = -1
-	, logit_phi1 = -1
-	, logit_W_asymp = -1
->>>>>>> 7bcfd66ba0ec1e9e4083af5b5240fd7db71ae02e
-	)
-      , log_nb_disp = c(postest=1)
-)
-)
+# opt_pars <- list(params=c(log_E0=2, log_beta0=-1, logit_mu = -1
+# 	, logit_phi1 = -1
+# 	, logit_W_asymp = -1
+# 	)
+#       , log_nb_disp = c(postest=1)
+# )
 
 sim_args <- list(ratemat_args = list(testify=TRUE, testing_time="report")) 
 
@@ -66,6 +46,7 @@ testify_calib <- do.call(calibrate_comb,
                                , use_DEoptim = FALSE
                                ## , testing_data = testdat
                                , data = dat
+                               , use_spline = FALSE
                                , opt_pars = opt_pars
                                , sim_args = sim_args
                                  ))
@@ -82,7 +63,7 @@ print(dd)
 
 gg2 <- (ggplot(dd,aes(date))
    + geom_line(aes(y=value))
-   + geom_point(data=res_list[[1]]$data,aes(date,value))
+   + geom_point(data=dat,aes(date,value))
    + ggtitle("postest")
 )
 
