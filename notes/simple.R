3094a64afb347042a15583155976d5e3e6913212
Jonathan Dushoff <dushoff@mcmaster.ca>
2020-08-12 22:41:36 -0400

    horrible experiments, likely answer

---
+++ notes/simple.R
@@ -0,0 +1,12 @@
+use_ode <- FALSE
+testwt_scale <- "none" ## or "N" or "sum_u"
+testing_intensity <- c(0.002)
+W_asymp <- c(0.01, 0.1,1)
+iso_t <- c(0,0.5,1)
+start <- as.Date("2020-01-01")
+end <- as.Date("2020-6-01")
+pop <- 1.5e7
+R0 <- 2.5
+Gbar <- 6
+set.seed(0807)
+keep_all <- FALSE
