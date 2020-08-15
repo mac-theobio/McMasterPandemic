2e8919e100c184cc29a3dfe7e2b4ed140c0bccfb
wzmli <wzmichael.li@gmail.com>
2020-08-15 12:13:55 -0400

    pushing simulation results

--- notes/simcalib_plot.R
+++ notes/simcalib_plot.R
@@ -10,7 +10,7 @@ print(head(dat))
 
 gg <- (ggplot(dat, aes(x=date, color=var))
 	+ geom_line(aes(y=value))
-	+ geom_point(aes(y=data))
+	+ geom_point(aes(y=data),alpha=0.1)
 	+ facet_grid(testing_intensity~keep_vars, scale="free")
 	+ scale_y_log10()
 )
