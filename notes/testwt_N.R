5fe57fbdf990a205595b0d28e8ccc31b6c43daeb
wzmli <wzmichael.li@gmail.com>
2020-08-17 01:40:06 -0400

    it works with realistic testing intensity

--- notes/testwt_N.R
+++ notes/testwt_N.R
@@ -2,11 +2,10 @@ use_ode <- FALSE
 testwt_scale <- "N"  ## experiment with rescaling testing weights
 testing_type <- c("constant","linear","logistic") ## we know constant is good, linear is kind of like logistic
 #testing_type <- c("linear")
-testing_intensity <- 0.001
 iso_t <- c(0,0.5,0.9,1)
 omega <- c(0.2,1)
-start <- as.Date("2020-01-01")
-end <- as.Date("2020-10-01")
+start <- as.Date("2020-03-01")
+end <- as.Date("2020-08-01")
 pop <- 1.5e7
 R0 <- 2.5
 Gbar <- 6
@@ -14,6 +13,7 @@ set.seed(0807)
 keep_vars <- c("death/H/postest")
 keep_all <- FALSE
 
-min_testing <- testing_intensity ##3e-5
-max_testing <- 0.01 ##2e-3
+min_testing <- 3e-5
+max_testing <- 2e-3
 
+testing_intensity <- min_testing
