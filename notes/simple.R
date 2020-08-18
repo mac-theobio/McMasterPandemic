133c71944dc122ef76e8d4ee6c598208f04a8c45
Jonathan Dushoff <dushoff@mcmaster.ca>
2020-08-17 20:57:55 -0400

    merge with Mike

--- notes/simple.R
+++ notes/simple.R
@@ -1,8 +1,8 @@
 use_ode <- FALSE
 testwt_scale <- "N" ## or "N" or "sum_u"
-testing_intensity <- c(0.01)
+testing_intensity <- c(0.02)
 testing_time="sample"
-W_asymp <- c(0.1, 1)
+W_asymp <- c(0.2, 1)
 iso_t <- c(0, 1)
 start <- as.Date("2020-01-01")
 end <- as.Date("2020-12-01")
