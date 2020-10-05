ac622a5c03252f0769a610f315edcdd4663fa6b1
Jonathan Dushoff <dushoff@mcmaster.ca>
2020-10-05 17:18:02 -0400

    banging heads against the wall

--- notes/spline_sim.R
+++ notes/spline_sim.R
@@ -16,25 +16,32 @@ first_date <- as.Date("2020-01-01")
 
 dd <- first_date -1 + 1:fitmax
 
-## Construct a spline
+## Reconstruct the spline fit
 bb <- coef(mod_bs)
-bt <-  exp(X %*% matrix(bb, ncol=1))
-print(bt)
-plot(bt)
-
+Rt <-  exp(X %*% matrix(bb, ncol=1))
+print(Rt)
+plot(Rt)
 print(params)
-params <- update(params, beta0=bt[[1]])
-params <- fix_pars(params, target=c(R0=R0))
+
+## Time-vary betas are actually Rs, so we set base R to 1
+params <- fix_pars(params, target=c(R0=1))
+print(params)
 summary(params)
 
-sims <- run_sim_loglin(params=params
+nullsim <- run_sim_loglin(params=params
+	, time_args=list(X_date=dd, X=X)
+	, sim_args=list(start_date=min(dd),end_date=max(dd))
+)
+
+sim <- run_sim_loglin(params=params
 	, extra_pars=list(time_beta=bb)
 	, time_args=list(X_date=dd, X=X)
 	, sim_args=list(start_date=min(dd),end_date=max(dd))
 )
 
-print(sims)
+print(sim)
 
-print(plot(sims$date,sims$report, log="y"))
+plot(nullsim$date,nullsim$report, log="y")
+lines(sim$date,sim$report)
 
 saveEnvironment()
