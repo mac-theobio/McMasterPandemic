f356680daf09f23ac305f0614b3c72917f875e3b
wzmli <wzmichael.li@gmail.com>
2020-10-14 10:01:22 -0400

    Autocommit

---
+++ notes/spline_fitplots.R
@@ -0,0 +1,20 @@
+library(McMasterPandemic)
+library(tidyverse)
+
+
+source("makestuff/makeRfuns.R") ## Will eventually be a package
+commandEnvironments() ## Read in any environments specified as dependencies
+makeGraphics()
+
+flist <- list.files(path="cachestuff/",pattern="_calib[.]")
+
+print(flist)
+
+plot_fits <- function(x){
+	modlist <- readRDS(paste0("cachestuff/",x))
+	print(plot(modlist$fit, data=modlist$fitdat))
+}
+
+sapply(flist,plot_fits)
+
+
