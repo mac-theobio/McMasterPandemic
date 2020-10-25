b07933b7f46861df21bad41257d4c478dc2e17b1
wzmli <wzmichael.li@gmail.com>
2020-10-25 18:55:50 -0400

    combining simulation results

--- notes/collect_spline_shape_calib.R
+++ notes/collect_spline_shape_calib.R
@@ -1,41 +1,40 @@
 library(McMasterPandemic)
 library(tidyverse)
 
-
 source("makestuff/makeRfuns.R") ## Will eventually be a package
 commandEnvironments() ## Read in any environments specified as dependencies
 ## makeGraphics()
 
-flist <- list.files(path="cachestuff/",pattern="spline_calib[.]")
+flist <- list.files(path="cachestuff/",pattern="spline_shape_calib[.]")
 
 print(flist)
 
 ## true beta0 is the same, it lives inside base_params
 
 tempmod <- readRDS(paste0("cachestuff/",flist[1]))
-
-
 base_params <- tempmod$fit$forecast_args$base_params
+
+
+print(coef(tempmod$fit, "fitted"))
 
 ## We expect the ratio of R/β to stay constant
 rmult <- get_R0(base_params)/base_params[["beta0"]]
+R0 <- get_R0(base_params)
 print(rmult)
-
-print(summary(tempmod$fit)$R0)
 
 ## Calculate time-varying betas
 btfun <- function(cc, X){
-	bt <- cc$params[["beta0"]] * exp(X %*% matrix(cc$time_beta, ncol=1))
+	bt <- exp(X %*% matrix(cc$time_beta, ncol=1))
 }
 
 collect_pars <- function(x){
 	modlist <- readRDS(paste0("cachestuff/",x))
-	mods <- c("fit","fitpen","fitE0","fitE0pen")
+	mods <- c("fit","fitpen")
 	df_funs <- function(seed,y){
 		tempmod <- modlist[[y]]
 		cc <- coef(tempmod, "fitted")
-		parsdf <- data.frame(beta0 = cc$params[["beta0"]]
-			, E0 = as.numeric(cc$params[2]) ## cannot say E0, it will give an error for mods without E0
+		parsdf <- data.frame(beta0 = base_params[["beta0"]]
+			, E0 = base_params[["E0"]] 
 #			, tbint = cc$time_beta[1]
 			, tb1 = cc$time_beta[1]
 			, tb2 = cc$time_beta[2]
@@ -78,7 +77,7 @@ true_pars_df <- data.frame(beta0 = base_params["beta0"]
 )
 
 
-combo_pars <- (bind_rows(true_pars_df, pars_df)
+combo_ss_pars <- (bind_rows(true_pars_df, pars_df)
 	%>% gather(key = "var", value = "value", -seed, -type, -mod, -spline_pen)
 )
 
@@ -86,15 +85,14 @@ combo_pars <- (bind_rows(true_pars_df, pars_df)
 
 collect_splines <- function(x){
 	modlist <- readRDS(paste0("cachestuff/",x))
-	mods <- c("fit","fitpen","fitE0","fitE0pen")
+	mods <- c("fit","fitpen")
 	df_funs <- function(seed,y){
 		tempmod <- modlist[[y]]
-		R0t <- summary(tempmod)[["R0"]]
+#		R0t <- summary(tempmod)[["R0"]]
 		cc <- coef(tempmod, "fitted")
 		spline_df <- data.frame(time = 1:nrow(X)
-		, bt = btfun(cc=cc,X=tempmod$forecast_args$time_args$X)/base_params[["beta0"]]
+		, Rt = btfun(cc=cc,X=tempmod$forecast_args$time_args$X)*R0
 		, seed = x
-		, Rt = R0t[-1]
 		, type = "sim"
 		, mod = y
 		)
@@ -110,16 +108,15 @@ spline_df <- bind_rows(lapply(flist,collect_splines))
 ## copied from spline_recalib.R 
 
 true_splines <- data.frame(time=1:nrow(X)
-	, bt = Rt
 	, Rt = Rt
 	, seed = NA
 	, type = "true"
 	, mod = "true"
 )
 
-spline_df <- bind_rows(spline_df, true_splines)
+spline_shape_df <- bind_rows(spline_df, true_splines)
 
-print(spline_df)
+print(spline_shape_df)
 
-saveVars(combo_pars, spline_df, rmult, base_params)
+saveVars(combo_ss_pars, spline_shape_df, rmult, base_params)
 
--- notes/spline_plots.R
+++ notes/spline_plots.R
@@ -5,7 +5,15 @@ source("makestuff/makeRfuns.R")
 commandEnvironments()
 makeGraphics()
 
-print(combo_pars)
+combo_pars$withb0 <- "yes"
+combo_ss_pars$withb0 <- "no"
+
+combo_pars <- bind_rows(combo_pars, combo_ss_pars)
+
+true_pars <- combo_pars %>% filter(type == "true")
+combo_pars <- combo_pars %>% filter(type != "true")
+
+spline_df2 <- bind_rows(spline_df, spline_shape_df)
 
 print(spline_df)
 
@@ -13,25 +21,36 @@ combo_pars <- (combo_pars
 	%>% mutate(spline_pen = ifelse(grepl("pen",mod),"yes","no")
 		, spline_pen = ifelse(type == "true", "no", spline_pen)
 		, model = ifelse(grepl("E0",mod),"withE0","withoutE0")
+		, model = ifelse(grepl("spline_shape",seed),paste0(model,"withoutB0"),paste0(model,"withB0"))
+		, model = ifelse(model == "withoutE0withoutB0", "none"
+			, ifelse(model == "withoutE0withB0", "B0"
+				, "E0B0")
+			)
 	)
 )
 
+spline_df2 <- (spline_df2
+	%>% mutate(model = ifelse(grepl("spline_shape",seed),"noB0","B0"))
+)
 
-gg <- (ggplot(combo_pars, aes(x=mod, y=value,color=mod))
-	+ geom_point()
+gg <- (ggplot(combo_pars, aes(x=mod, y=value,color=model))
+	+ geom_point(position= position_dodge(width = 0.9))
 	+ facet_wrap(~var,scale="free")
+	+ geom_hline(data=true_pars, aes(yintercept=value))
 	+ scale_alpha_manual(values=c(1,0.3,0.3,0.3,0.3))
-	+ scale_color_manual(values=c("blue","blue","red","red","black"))
+	+ scale_color_manual(values=c("black","red","blue"))
 	+ coord_flip()
 )
 
 print(gg)
 
-print(spline_df)
+print(unique(spline_df$mod))
+print(unique(spline_df2$mod))
 
-ggsplines <- (ggplot(spline_df,aes(time,color=mod,group=seed))
-	+ facet_wrap(~mod)
-	+ scale_color_manual(values=c("blue","blue","red","red","black"))
+
+ggsplines <- (ggplot(spline_df2,aes(time,color=mod,group=seed))
+	+ facet_grid(model~mod)
+#	+ scale_color_manual(values=c("blue","red"))
 	+ ylim(c(0,10))
 )
 
