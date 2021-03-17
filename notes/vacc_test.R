3bbc5110add2175bc6d7d4ee6672447d5c6bee01
wzmli <wzmichael.li@gmail.com>
2021-03-16 20:00:44 -0500

    updating single pop vacc model

--- notes/vacc_test.R
+++ notes/vacc_test.R
@@ -1,7 +1,7 @@
 library(McMasterPandemic)
 library(cowplot)
 devtools::load_all("..")
-p0 <- read_params("ICU1.csv")
+p0 <- fix_pars(read_params("ICU1.csv"), target = c(R0=1.5,Gbar=6))
 p1 <- update(p0, vacc=0.001)
 p2 <- update(p0, vacc=1e-7)
 
@@ -13,15 +13,17 @@ r2 <- run_sim(params=p1,
               start_date="01-Mar-2020",
               end_date="01-Jul-2020")
 
-tv <- data.frame(Date=c("01-Apr-2020",
-                        "30-Apr-2020"),
-                 Symbol=rep("vacc",2),
-                 Relative_value=c(1e5,1e6))
+tv <- data.frame(Date=c("15-Mar-2020"
+    , "30-Mar-2020"
+    )
+    , Symbol=rep("vacc",1)
+    , Relative_value=c(1e5,2e5)
+)
 r3 <- run_sim(params=p2,
               start_date="01-Mar-2020",
               end_date="01-Jul-2020",
               params_timevar=tv)
-r4 <- run_sim(params=update(p2,vacc=1e-7),
+r4 <- run_sim(params=update(p2,vacc=1e-2),
               start_date="01-Mar-2020",
               end_date="01-Jul-2020")
 
