90619e029af384a5a2c9a494c61cdfd64f4f73f2
Weiguang Guan <guanw@sharcnet.ca>
2021-07-31 23:37:58 +0000

    TMB version produces the same result as example(do_step).

--- notes/TMB_do_step.cpp
+++ notes/TMB_do_step.cpp
@@ -49,6 +49,165 @@ std::vector<std::string> exclude_states(
 }
 
 ///////////////////////////////////////////////////////////////////////////////
+template<class Type>
+bool has_testing(
+    const vector<Type>* state,
+    const vector<Type>* params= NULL,
+    const Eigen::SparseMatrix<Type>* ratemat= NULL
+)
+{
+    return false;
+/*
+    if (params!=NULL) {
+        std::vector<std::string>::iteractor it = \
+            std::find(paramNames.begin(), paramNames.end(), "testing_intensity");
+        if (it!=paramNames.end())
+            return (paramName2IndexMap["testing_intensity"]=0)
+        else
+            return true;
+    }
+
+    // if (ratemat) // in our simplified case, ratemat's row/col names are the same as state's names
+     
+        
+has_testing <- function(state, params = NULL, ratemat = NULL) {
+    if (!is.null(params)) {
+        return("testing_intensity" %in% names(params) && params[["testing_intensity"]] != 0)
+    }
+    if (!is.null(ratemat)) {
+        return(any(grepl("_t$", rownames(ratemat))))
+    }
+    return(any(grepl("_t$", names(state))))
+*/
+}
+
+///////////////////////////////////////////////////////////////////////////////
+template<class Type>
+std::vector<int> pfun(
+    std::string& from, 
+    std:: string& to, 
+    Eigen::SparseMatrix<Type>& mat, 
+    bool value = FALSE, 
+    bool recycle = FALSE) 
+{
+    std::vector<int> result;
+
+    result.push_back(std::find(stateNames.begin(), stateNames.end(), from) - stateNames.begin());
+    result.push_back(std::find(stateNames.begin(), stateNames.end(), to) - stateNames.begin());
+
+    return result;
+
+/*
+    pfun_method <- getOption("macpan_pfun_method", "startsWith")
+    find_pos_grep <- function(tag, x) {
+        grep(sprintf("^%s(_|$)", tag), x, value = value)
+    }
+    find_pos_startsWith <- function(tag, x) {
+        nt <- nchar(tag)
+        r <- which(startsWith(x, tag)) ## subset quickly
+        ## test remainder for _ or $
+        r <- r[(substr(x[r], nt + 1, nt + 1) == "_") |
+            nchar(x[r]) == nt]
+        if (!value) {
+            return(r)
+        }
+        return(x[r])
+    }
+
+    if (pfun_method == "both") {
+        from_pos <- find_pos_grep(from, rownames(mat))
+        to_pos <- find_pos_grep(to, colnames(mat))
+        from_pos_sw <- find_pos_startsWith(from, rownames(mat))
+        to_pos_sw <- find_pos_startsWith(to, colnames(mat))
+        stopifnot(all(from_pos == from_pos_sw) && all(to_pos == to_pos_sw))
+    } else {
+        find_pos <- switch(pfun_method,
+            startsWith = find_pos_startsWith,
+            grep = find_pos_grep
+        )
+        from_pos <- find_pos(from, rownames(mat))
+        to_pos <- find_pos(to, colnames(mat))
+    }
+
+    nf <- length(from_pos)
+    nt <- length(to_pos)
+    if (recycle && (nt == 1 || nf == 1)) {
+        from_pos <- rep(from_pos, length.out = max(nt, nf))
+        to_pos <- rep(to_pos, length.out = max(nt, nf))
+    }
+    if (!(length(to_pos) == length(from_pos) &&
+        length(to_pos) > 0 && length(from_pos) > 0)) { ## must be positive
+        stop(sprintf(
+            "to_pos, from_pos don't match: from_pos=%s, to_pos=%s",
+            paste(colnames(mat)[from_pos], collapse = ", "),
+            paste(rownames(mat)[to_pos], collapse = ", ")
+        ))
+    }
+
+    return(cbind(from_pos, to_pos))
+*/
+}
+
+///////////////////////////////////////////////////////////////////////////////
+template<class Type>
+Type update_foi(
+    vector<Type>& state, 
+    vector<Type>& params, 
+    vector<Type>& beta // *** This is a trouble-maker for R-C++ conversion as beta could be 
+         // either a vector or a matrix. Here lets assume it is vector
+) 
+{
+    assert(state.size()==beta.size());
+    
+    Type foi = 0;
+    for (int i = 0; i < state.size(); i++) {
+        foi += state(i)*beta(i);
+    }
+
+    return foi;
+
+/*
+    ## update infection rate
+    if (is.matrix(beta)) {
+        ## e.g. beta is a matrix when the transmission parameters are age-structured
+        if (length(state) != ncol(beta)) stop("number of columns of beta must match length of state vector")
+        foi <- beta %*% state
+    } else {
+        if (length(state) != length(beta)) {
+            stop("length of state and beta are not the same")
+        }
+        foi <- sum(state * beta)
+    }
+    if (has_zeta(params)) {
+        if (has_age(params)) stop("phenomenological heterogeneity (zeta != 0) untested with age-structed par
+ams")
+        ## suppose zeta is a vector zeta1, zeta2, zeta3, ...
+        ##  we also need parameters   zeta_break1, zeta_break2 (0<zbi<1)
+        ##  one *fewer* break parameter than zeta_i value
+        ## if 0< S/N < zeta_break1   -> zeta1
+        ##  zeta_break1 < S/N < zeta_break2 -> zeta2
+        ## ...
+        ##  zeta_breakx < S/N < 1  -> zetax
+        Susc_frac <- 1 / params[["N"]] * sum(state[grep("^S_?", names(state))])
+        if (any(grepl("zeta[0-9]", names(params)))) {
+            zeta <- with(
+                as.list(params),
+                if (Susc_frac < zeta_break) zeta1 else zeta2
+            )
+        }
+        ## alternately could just make it a vector
+        ## ... but this messes with age-structured stuff
+        ## if (length(zeta)>0) {
+        ## ...
+        ## }
+        ## ???? het at pop level or sub-category level??
+        foi <- foi * with(as.list(params), Susc_frac^zeta)
+    }
+    return(foi)
+*/
+}
+
+///////////////////////////////////////////////////////////////////////////////
 // Helper function update_ratemat (unfinished!!!)
 template<class Type>
 Eigen::SparseMatrix<Type> update_ratemat(
@@ -59,18 +218,84 @@ Eigen::SparseMatrix<Type> update_ratemat(
 {
     Eigen::SparseMatrix<Type> result = ratemat;
 
+    if (has_testing(&state, &params)) { // false in the simplied test case, so skip
+        //aa <- c("wtsvec", "posvec", "testing_time")
+        //saved_attrs <- setNames(lapply(aa, attr, x = ratemat), aa)
+    }
+    //## update testing flows. DO THIS FIRST, before updating foi: **** assignment via cbind() to Matrix objects loses attributes???
+    if (has_testing(&state)) { // false in the simplied test case, so skip
+        /*
+        testing_time <- attr(ratemat, "testing_time") ## ugh  (see **** above)
+        ## positions of untested, positive-waiting, negative-waiting compartments
+        ## (flows from _n, _p to _t, or back to _u, are always at per capita rate omega, don't need
+        ##  to be updated for changes in state)
+        ## FIXME: backport to testify?
+        u_pos <- grep("_u$", rownames(ratemat))
+        p_pos <- grep("_p$", rownames(ratemat))
+        n_pos <- grep("_n$", rownames(ratemat))
+        ## original/unscaled prob of positive test by compartment, testing weights by compartment
+        posvec <- attr(ratemat, "posvec")
+        if (is.null(posvec)) stop("expected ratemat to have a posvec attribute")
+        wtsvec <- attr(ratemat, "wtsvec")
+        if (is.null(wtsvec)) stop("expected ratemat to have a wtsvec attribute")
+        ## scaling ...
+        testing_intensity <- params[["testing_intensity"]]
+        testing_tau <- params[["testing_tau"]]
+        N0 <- params[["N"]]
+        W <- sum(wtsvec * state[u_pos])
+        sc <- switch(testwt_scale,
+            none = 1,
+            N = N0 / W,
+            sum_u = sum(state[u_pos]) / W,
+            sum_smooth = {
+                rho <- testing_intensity
+                tau <- testing_tau
+                tau * N0 / (tau * W + rho * N0)
+                ## NOTE 'smoothing' doc has numerator rho*tau*N0,
+                ## but testing intensity (rho) is included in ratemat
+                ## calculation below ...
+            }
+        )
+        ratemat[cbind(u_pos, n_pos)] <- testing_intensity * sc * wtsvec * (1 - posvec)
+        ratemat[cbind(u_pos, p_pos)] <- testing_intensity * sc * wtsvec * posvec
+        if (testing_time == "sample") {
+            N_pos <- which(rownames(ratemat) == "N")
+            P_pos <- which(rownames(ratemat) == "P")
+            ratemat[cbind(u_pos, N_pos)] <- ratemat[cbind(u_pos, n_pos)]
+            ratemat[cbind(u_pos, P_pos)] <- ratemat[cbind(u_pos, p_pos)]
+        }
+        */
+    }
+
+/*
+    ## update FOIs
+    ratemat[pfun("S", "E", ratemat)] <- update_foi(state, params, make_beta(state, params))
+
+    ## update vaccine allocation rates
+    if (has_vax(params)) {
+        ratemat <- add_updated_vaxrate(state, params, ratemat)
+    }
+
+    ## ugh, restore attributes if necessary
+    if (inherits(ratemat, "Matrix") && has_testing(params)) {
+        for (a in aa) {
+            attr(ratemat, a) <- saved_attrs[[a]]
+        }
+    }
+    return(ratemat)
+*/
+    return result;
+
+// Below is Ben's version of update_ratemat(...). Using it won't generate the same state as
+// example("do_step")
+/*
+    Eigen::SparseMatrix<Type> result = ratemat;
+
     std::vector<int> inf_ind;
     int transm_ind;
     std::vector<int> transm_wt_ind;
     std::vector<int> foi_ind;
-/*
-    inf_ind = grep("I[a-z]", names(s)),
-                            transm_ind = which(names(p) == "beta0"),
-                            ## fragile! assumes same order as state
-                            transm_wt_ind = grep("C[a-z]", names(p)),
-                            foi_ind = c(which(rownames(M) == "S"),
-                                        which(colnames(M) == "E"))
-*/
+
     // We may need to check inf_ind, transm_ind, transm_wt_ind have valid values after
     // executing the following code block
     for (int i= 0; i< stateNames.size(); i++) 
@@ -104,6 +329,7 @@ Eigen::SparseMatrix<Type> update_ratemat(
     result.coeffRef(foi_ind.at(0), foi_ind.at(1)) = foi;
 
     return result;
+*/
 }
 
 ///////////////////////////////////////////////////////////////////////////////
