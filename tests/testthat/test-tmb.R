7bd5f1224659bd8b22e392baf6415454d41d985b
stevencarlislewalker <steve.walker@utoronto.ca>
2021-09-08 16:40:44 -0400

    test trajectories generated by spec v0.0.2

--- tests/testthat/test-tmb.R
+++ tests/testthat/test-tmb.R
@@ -116,41 +116,8 @@ test_files = "../../inst/tmb/"
 
 params = read_params('ICU1.csv')
 state = make_state(params = params)
-test_model = (
-  init_model(params, state)
-  %>% add_rate("E", "Ia", ~ (alpha) * (sigma))
-  %>% add_rate("E", "Ip", ~ (1 - alpha) * (sigma))
-  %>% add_rate("Ia", "R", ~ (gamma_a))
-  %>% add_rate("Ip", "Im", ~ (mu) * (gamma_p))
-  %>% add_rate("Ip", "Is", ~ (1 - mu) * (gamma_p))
-  %>% add_rate("Im", "R", ~ (gamma_m))
-  %>% add_rate("Is", "H", ~ (1 - nonhosp_mort) * (phi1) * (gamma_s))
-  %>% add_rate("Is", "ICUs", ~ (1 - nonhosp_mort) * (1 - phi1) * (1 - phi2) * (gamma_s))
-  %>% add_rate("Is", "ICUd", ~ (1 - nonhosp_mort) * (1 - phi1) * (phi2) * (gamma_s))
-  %>% add_rate("Is", "D", ~ (nonhosp_mort) * (gamma_s))
-  %>% add_rate("ICUs", "H2", ~ (psi1))
-  %>% add_rate("ICUd", "D", ~ (psi2))
-  %>% add_rate("H2", "R", ~ (psi3))
-  %>% add_rate("H", "R", ~ (rho))
-  %>% add_rate("Is", "X", ~ (1 - nonhosp_mort) * (phi1) * (gamma_s))
-  %>% add_rate("S",  "E", ~
-               (Ia) * (beta0) * (1/N) * (Ca) +
-               (Ip) * (beta0) * (1/N) * (Cp) +
-               (Im) * (beta0) * (1/N) * (Cm) * (1-iso_m) +
-               (Is) * (beta0) * (1/N) * (Cs) * (1-iso_m))
-  %>% add_parallel_accumulators(c("X", "N", "P", "V"))
-  %>% add_tmb_indices()
-)
+M = McMasterPandemic::make_ratemat(state, params, sparse = TRUE)
 
-
-
-spec_version = "0.0.2"
-options(MP_flex_spec_version = spec_version)
-context(paste0("tests for spec version: ", spec_version))
-test_files = "../../inst/tmb/"
-
-params = read_params('ICU1.csv')
-state = make_state(params = params)
 test_model = (
   init_model(params, state)
   %>% add_rate("E", "Ia", ~ (alpha) * (sigma))
@@ -176,3 +143,66 @@ test_model = (
   %>% add_parallel_accumulators(c("X", "N", "P", "V"))
   %>% add_tmb_indices()
 )
+
+indices = test_model$tmb_indices$make_ratemat_indices
+update_indices = test_model$tmb_indices$update_ratemat_indices
+par_accum_indices = test_model$tmb_indices$par_accum_indices
+
+dd <- MakeADFun(data = list(state = c(test_model$state),
+                            from = indices$from,
+                            to = indices$to,
+                            count = indices$count,
+                            spi = indices$spi,
+                            modifier = indices$modifier,
+                            update_from = update_indices$from,
+                            update_to = update_indices$to,
+                            update_count = update_indices$count,
+                            update_spi = update_indices$spi,
+                            update_modifier = update_indices$modifier,
+                            par_accum_indices = par_accum_indices
+),
+parameters = list(params=c(test_model$params)),
+DLL=basename(dll))
+
+tmb_sparse_ratemat = dd$report()$ratemat
+tmb_concatenated_state_vector = dd$report()$concatenated_state_vector
+
+tmb_traj = (state
+  %>% c(tmb_concatenated_state_vector)
+  %>% matrix(length(state), 4, dimnames = list(names(state), 1:4))
+  %>% t
+  %>% as.data.frame
+)
+r_traj = run_sim_range(
+  params = params, state = state, nt = 4,
+  step_args = list(do_hazard = FALSE))[,names(state)]
+
+r_dense_ratemat = as.matrix(M)
+tmb_dense_ratemat = as.matrix(tmb_sparse_ratemat)
+r_sparse_ratemat = M
+dimnames(tmb_sparse_ratemat) <-
+  dimnames(r_sparse_ratemat) <-
+  dimnames(tmb_dense_ratemat) <-
+  dimnames(r_dense_ratemat) <- NULL
+
+test_that("rate matrix types match", {
+  expect_equal(
+    class(tmb_sparse_ratemat),
+    class(r_sparse_ratemat))
+})
+
+test_that("rate matrix dimensions match", {
+  expect_equal(
+    tmb_sparse_ratemat@Dim,
+    r_sparse_ratemat@Dim)
+})
+
+test_that("matrix elements are equal", {
+  expect_equal(
+    tmb_dense_ratemat,
+    r_dense_ratemat)
+})
+
+test_that("simulated state trajectories are equal", {
+  expect_equal(tmb_traj, r_traj)
+})
