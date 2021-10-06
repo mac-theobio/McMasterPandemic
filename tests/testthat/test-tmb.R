cbb2341d120c96311f187fe6c0451bcaaa27a385
stevencarlislewalker <steve.walker@utoronto.ca>
2021-10-05 20:41:42 -0400

    test tmb under vaccination simulation

--- tests/testthat/test-tmb.R
+++ tests/testthat/test-tmb.R
@@ -533,3 +533,138 @@ test_that('simple models still work when structure is allowed', {
     )
     compare_sims(r_sim, tmb_sim)
 })
+
+test_that("simple vaccination model in TMB matches and is faster than existing R model", {
+    options(macpan_pfun_method = "grep")
+    params <- read_params("ICU1.csv")
+    state <- make_state(params = params)
+    vax_params <- expand_params_vax(
+        params = params,
+        model_type = "twodose"
+    )
+    vax_state <- expand_state_vax(
+        x = state,
+        model_type = "twodose",
+        unif = FALSE
+    )
+
+    # problem dimensions
+    (epi_states = c(attr(vax_state, "epi_cat")))
+    (asymp_cat = c("S", "E", "Ia", "Ip", "R"))
+    (vax_cat = c(attr(vax_state, "vax_cat")))
+    (dose_from = rep(asymp_cat, 2))
+    (dose_to = c(asymp_cat, rep("V", 5)))
+
+    # Specify structure of the force of infection calculation
+    Istate = (McMasterPandemic:::expand_names(
+        c('Ia', 'Ip', 'Im', 'Is'),   # = Icats
+        attr(vax_params, 'vax_cat')) # = vax_cats
+        %>% struc
+    )
+    baseline_trans_rates =
+        struc(
+            'Ca',
+            'Cp',
+            '(1 - iso_m) * (Cm)',
+            '(1 - iso_s) * (Cs)') *
+        struc('(beta0) * (1/N)')
+    vax_trans_red = struc_block(struc(
+        '1',
+        '1',
+        '(1 - vax_efficacy_dose1)',
+        '(1 - vax_efficacy_dose1)',
+        '(1 - vax_efficacy_dose2)'),
+        row_times = 1, col_times = 5)
+
+    alpha   = c("alpha", "alpha", "vax_alpha_dose1", "vax_alpha_dose1", "vax_alpha_dose2")
+    mu      = c("mu",    "mu",    "vax_mu_dose1",    "vax_mu_dose1",    "vax_mu_dose2")
+    sigma   = struc("sigma")
+    gamma_p = struc("gamma_p")
+    E_to_Ia_rates  = struc(           alpha ) * sigma
+    E_to_Ip_rates  = struc(complement(alpha)) * sigma
+    Ip_to_Im_rates = struc(              mu ) * gamma_p
+    Ip_to_Is_rates = struc(complement(   mu)) * gamma_p
+
+    test_model <- (init_model(
+        vax_params, vax_state,
+        start_date = "2021-09-09", end_date = "2021-10-09"
+    )
+
+    # Flow within vaccination categories,
+    # with constant rates across categories
+    %>% rep_rate("Ia",   "R",    ~                      (gamma_a))
+    %>% rep_rate("Im",   "R",    ~                      (gamma_m))
+    %>% rep_rate("Is",   "D",    ~ (    nonhosp_mort) * (gamma_s))
+    %>% rep_rate("Is",   "H",    ~ (1 - nonhosp_mort) * (gamma_s) * (    phi1))
+    %>% rep_rate("Is",   "X",    ~ (1 - nonhosp_mort) * (gamma_s) * (    phi1))
+    %>% rep_rate("Is",   "ICUs", ~ (1 - nonhosp_mort) * (gamma_s) * (1 - phi1) * (1 - phi2))
+    %>% rep_rate("Is",   "ICUd", ~ (1 - nonhosp_mort) * (gamma_s) * (1 - phi1) * (    phi2))
+    %>% rep_rate("ICUs", "H2",   ~                                  (    psi1))
+    %>% rep_rate("ICUd", "D",    ~                                  (    psi2))
+    %>% rep_rate("H2",   "R",    ~                                  (    psi3))
+    %>% rep_rate("H",    "R",    ~ (rho))
+
+    # Flow within vaccination categories,
+    # with rates that depend on category
+    # (see struc objects created above)
+    %>% vec_rate("E", "Ia",  E_to_Ia_rates)
+    %>% vec_rate("E", "Ip",  E_to_Ip_rates)
+    %>% vec_rate("Ip", "Im", Ip_to_Im_rates)
+    %>% vec_rate("Ip", "Is", Ip_to_Is_rates)
+
+    # Vaccination Response Rates
+    %>% add_rate("R_vaxdose1", "R_vaxprotect1",  ~ (vax_response_rate_R))
+    %>% add_rate("R_vaxdose2", "R_vaxprotect2",  ~ (vax_response_rate_R))
+    %>% add_rate("S_vaxdose1", "S_vaxprotect1",  ~ (vax_response_rate))
+    %>% add_rate("S_vaxdose2", "S_vaxprotect2",  ~ (vax_response_rate))
+
+    # Forces of Infection
+    %>% vec_rate(
+        "S" %_% vax_cat,
+        "E" %_% vax_cat,
+        kronecker(vax_trans_red, t(baseline_trans_rates)) %*% Istate
+    )
+
+    # Sums across vaccination categories
+    %>% add_state_param_sum("asymp_unvax_N",       asymp_cat %_% "unvax")
+    %>% add_state_param_sum("asymp_vaxprotect1_N", asymp_cat %_% "vaxprotect1")
+
+    # Flow among vaccination categories
+    # (see dose_* above for epi states that are involved)
+    %>% rep_rate(
+        dose_from %_% 'unvax',
+        dose_to   %_% 'vaxdose1',
+        ~ (    vax_prop_first_dose) * (vax_doses_per_day) * (1 / asymp_unvax_N))
+    %>% rep_rate(
+        dose_from %_% 'vaxprotect1',
+        dose_to   %_% 'vaxdose2',
+        ~ (1 - vax_prop_first_dose) * (vax_doses_per_day) * (1 / asymp_vaxprotect1_N))
+
+    # Technical steps
+    %>% add_parallel_accumulators(c('V' %_% vax_cat, 'X' %_% vax_cat))
+    %>% add_tmb_indices()
+    )
+    tmb_strt = Sys.time()
+    tmb_sim <- run_sim(
+        params = vax_params, state = vax_state,
+        start_date = "2021-09-09", end_date = "2021-10-09",
+        condense = FALSE,
+        step_args = list(do_hazard = TRUE),
+        flexmodel = test_model,
+        use_flex = TRUE
+    )
+    tmb_nd = Sys.time()
+    r_strt = Sys.time()
+    r_sim = run_sim(
+        params = vax_params, state = vax_state,
+        start_date = "2021-09-09", end_date = "2021-10-09",
+        condense = FALSE,
+        step_args = list(do_hazard = TRUE)
+    )
+    r_nd = Sys.time()
+    tmb_speed = as.numeric(tmb_nd - tmb_strt)
+    r_speed = as.numeric(r_nd - r_strt)
+    print(paste0('tmb speed-up: ', round(r_speed/tmb_speed), 'x'))
+    expect_lt(tmb_speed, r_speed)
+    compare_sims(r_sim, tmb_sim)
+})
