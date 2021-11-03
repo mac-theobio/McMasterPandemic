802fce621c46c77b4e3ba5fdcef7ac68474b2b36
stevencarlislewalker <steve.walker@utoronto.ca>
2021-11-02 21:40:12 -0400

    example for buildings tests of automatic jacobian

---
+++ notes/indexing/jacobian_test.R
@@ -0,0 +1,22 @@
+library(McMasterPandemic)
+library(tools)
+library(dplyr)
+
+cpp <- file.path("inst/tmb/0.1.1", "macpan.cpp")
+dll <- file_path_sans_ext(cpp)
+options(MP_flex_spec_dll = basename(dll))
+
+compile(cpp)
+dyn.load(dynlib(dll))
+
+params <- read_params("ICU1.csv")
+state <- make_state(params = params)
+start_date = "2021-05-10"
+end_date = "2021-12-10"
+model <- make_base_model(
+  params, state,
+  start_date = start_date, end_date = end_date,
+  step_args = list(do_hazard = TRUE)
+)
+report <- tmb_fun(model)$report()
+report$j
