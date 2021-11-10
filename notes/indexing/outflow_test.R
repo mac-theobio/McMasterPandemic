e6998eab1bbf25a6fdb7a10f4e2950c8d38cbb3e
stevencarlislewalker <steve.walker@utoronto.ca>
2021-11-10 15:27:26 -0500

    code to create outflow data structure example

---
+++ notes/indexing/outflow_test.R
@@ -0,0 +1,20 @@
+library(McMasterPandemic)
+library(tools)
+library(dplyr)
+
+cpp <- file.path("inst/tmb/0.1.1", "macpan.cpp")
+dll <- file_path_sans_ext(cpp)
+options(MP_flex_spec_dll = basename(dll))
+
+compile(cpp)
+dyn.load(dynlib(dll))
+
+params <- read_params("ICU1.csv")
+state <- make_state(params = params)
+start_date = "2021-05-10"
+end_date = "2021-12-10"
+model <- make_base_model(
+  params, state,
+  start_date = start_date, end_date = end_date
+)
+model$do_hazard = FALSE
