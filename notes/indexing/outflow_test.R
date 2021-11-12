76f5c1b2d1be2920e90e3dc7dd5913de40a82e98
stevencarlislewalker <steve.walker@utoronto.ca>
2021-11-12 12:56:49 -0500

    clean up outflow_test

--- notes/indexing/outflow_test.R
+++ notes/indexing/outflow_test.R
@@ -20,39 +20,3 @@ model <- make_base_model(
 model$do_hazard = FALSE
 
 report <- tmb_fun(model)$report()
-i = model$tmb_indices$initialization_mapping$all_to_eigen_idx
-jac = report$j[i, i]
-evec = eigen(jac)$vectors[,1]
-evec / sum(evec)
-
-
-report$eigenvec / sum(report$eigenvec)
-
-
-
-
-lapply(model$tmb_indices, names)
-
-
-model$tmb_indices$make_ratemat_indices$from
-drop_pattern = '(S|Is)'
-nms = c(names(state), names(params)) # TODO -- need to add sums here too
-new_nms = grep(drop_pattern, nms, value = TRUE, invert = TRUE)
-McMasterPandemic:::find_vec_indices(
-  nms[model$tmb_indices$make_ratemat_indices$spi],
-  setNames(new_nms, new_nms))
-
-model$tmb_indices$outflow
-model$tmb_indices$linearized_outflow
-
-lapply(outflow, function(o) {
-  list(
-    state = grep(o$state_patterns, rownames(ratemat)),
-    flow = grep(o$flow_state_patterns, colnames(ratemat))
-  )
-})
-
-length(model$linearized_outflow)
-length(model$tmb_indices$linearized_outflow)
-# Reformat linearized_outflow
-
