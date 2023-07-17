3a2f54fc1b4c27920fd5ba6b1cd993e0004d2daa
Irena Papst <irena.papst@phac-aspc.gc.ca>
2023-07-17 14:22:11 -0400

    enable `forecast_stan()` to just return the simulation over the calibration period

--- R/tmbstan_tools.R
+++ R/tmbstan_tools.R
@@ -88,7 +88,7 @@ traceplot_stan = function(
 #' Forecast ensemble using STAN fit
 #'
 #' @param model_fit the output of [calibrate_stan()].
-#' @param days_to_forecast the number of days to forecast in the future.
+#' @param days_to_forecast numeric. (optional) the number of days to forecast in the future. if NULL, just simulate over the calibration period.
 #' @param params_timevar data frame. (optional) time-varying parameters for forecast period. must have columns `Date`, `Symbol`, `Value`, and `Type`. see [here](https://canmod.github.io/macpan-book/time-varying-parameters.html#model-of-piece-wise-time-variation) for more details. if `NULL`, will simulate without time-varying parameter changes in the forecast period.
 #' @param parallel logical. should the ensemble simulation be run in parallel?
 #' @param n_cores integer. number of cores to use for parallel simulation.
@@ -99,7 +99,7 @@ traceplot_stan = function(
 #' @examples
 forecast_stan = function(
   model_fit,
-  days_to_forecast,
+  days_to_forecast = NULL,
   params_timevar = NULL,
   parallel = TRUE,
   n_cores = 2
@@ -123,11 +123,14 @@ forecast_stan = function(
 
   # update model with forecast info
 
-  model = (model
+  if(!is.null(days_to_forecast)){
+    model = (model
     # extend simulation end date
-    %>% McMasterPandemic::extend_end_date(
-     days_to_extend = days_to_forecast
-  ))
+      %>% McMasterPandemic::extend_end_date(
+        days_to_extend = days_to_forecast
+      )
+    )
+  }
 
   if(!is.null(params_timevar)){
     model = (model
